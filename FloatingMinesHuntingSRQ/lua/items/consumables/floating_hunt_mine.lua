b8852a89d924739d7bf90119234fffc6026a5d1b
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-02-24 02:00:28 +0300

    FloatingMinesHuntingSRQ.

--- FloatingMinesHuntingSRQ/lua/items/consumables/floating_hunt_mine.lua
+++ FloatingMinesHuntingSRQ/lua/items/consumables/floating_hunt_mine.lua
@@ -27,8 +27,8 @@ function floating_hunt_mine:init()
     self.sm:ChangeState("armed")
     self.entered = 0
     
-    self.hunt_range = self.data:GetFloatOrDefault( "hunt_range", 30.0 )
-    self.hunt_move_speed = self.data:GetFloatOrDefault( "hunt_move_speed", 20.0 )
+    self.hunt_range = self.data:GetFloatOrDefault( "hunt_range", 20.0 )
+    self.hunt_move_speed = self.data:GetFloatOrDefault( "hunt_move_speed", 5.0 )
 
     self.huntSM = self:CreateStateMachine()
     self.huntSM:AddState( "hunt", { execute="OnHuntExecute" } )
@@ -41,6 +41,7 @@ function floating_hunt_mine:OnHuntExecute( state )
     local target = FindClosestEntity( self.entity, allenemies )
 
     if target == INVALID_ID then
+        MoveService:StopMove( self.entity )
         return
     end
 
@@ -52,6 +53,12 @@ function floating_hunt_mine:FindEntitiesByTeamInRadius( origin, radius, team )
     local predicate = 
     {
         filter = function(entity)
+            
+            if ( not EntityService:IsAlive( entity ) ) then
+            
+                return false
+            end
+        
             if ( not EntityService:IsInTeamRelation(origin, entity, "hostility") ) then
             
                 return false
@@ -119,7 +126,7 @@ end
 function floating_hunt_mine:OnExplodeStart( state )
     EffectService:AttachEffects( self.entity, "mine_armed" )
     state:SetDurationLimit( self.delay )
-    self.position = EntityService:GetPosition( self.entity )
+    self.positionY = EntityService:GetPosition( self.entity ).y
     EntityService:RemoveComponent( self.entity, "HealthComponent")
 end
 
@@ -128,8 +135,12 @@ function floating_hunt_mine:OnExplodeExecute( state )
     if ( state:GetDurationLimit() ~= 0 ) then
         progres = state:GetDuration() / state:GetDurationLimit()
     end
+    
     EntityService:SetGraphicsUniform( self.entity, "cGlowFactor", progres * 10 )
-    EntityService:SetPosition( self.entity, self.position.x, self.position.y  + self.explode_height * (progres * progres) , self.position.z )
+    
+    local position = EntityService:GetPosition( self.entity )
+    EntityService:SetPosition( self.entity, position.x, self.positionY  + self.explode_height * (progres * progres) , position.z )
+    
     EntityService:Rotate( self.entity, 0.0, 1.0, 0.0, 25.0 * (progres * progres) )
 end
 
