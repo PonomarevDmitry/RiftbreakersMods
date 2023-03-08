8f780bc11cd7bec77463bab6d5992eb285853099
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-03-08 13:05:48 +0300

    MoreWeaponsConsumablesSkills. Logging command.

--- MoreWeaponsConsumablesSkills/lua/newconsumables_bind_autoexec.lua
+++ MoreWeaponsConsumablesSkills/lua/newconsumables_bind_autoexec.lua
@@ -4,6 +4,10 @@ ConsoleService:RegisterCommand( "activate_slot_request", function( args )
         return 
     end
 
+    local slotName = args[1]
+
+    LogService:Log("ExecuteCommand activate_slot_request " .. tostring(slotName) )
+
     if ( PlayerService == nil or ItemService == nil ) then
         return
     end
@@ -11,36 +15,54 @@ ConsoleService:RegisterCommand( "activate_slot_request", function( args )
     local player = PlayerService:GetPlayerControlledEnt(0)
 
     if ( player == nil ) then
+        LogService:Log("activate_slot_request " .. tostring(slotName) .. " player nil" )
         return
     end
-
-    local slotName = args[1]
 
     local slotItem = ItemService:GetEquippedItem( player, slotName )
 
     if ( slotItem == nil ) then
+        LogService:Log("activate_slot_request " .. tostring(slotName) .. " slotItem nil" )
         return
     end
 
     local slotItemType = ItemService:GetItemType( slotItem )
     
     if ( not ItemService:CanActivateItemSlot( player, slotName, slotItemType ) ) then
+        LogService:Log("activate_slot_request " .. tostring(slotName) .. " CanActivateItemSlot false" )
         return
     end
+
+    --LogService:Log("activate_slot_request " .. tostring(slotName) .. " ActivateEquipmentSlotRequest" )
+    --QueueEvent("ActivateEquipmentSlotRequest", player, slotName, "" )
+    --QueueEvent("DeactivateEquipmentSlotRequest", player, slotName, true )
     
+
+    LogService:Log("activate_slot_request " .. tostring(slotName) .. " UseEquippedItem" )
     ItemService:UseEquippedItem( player, slotName )
     ItemService:StopUsingEquippedItem( player, slotName )
     
 end)
 
 --JUMP_SKILL
---DODGE_ROLL_SKILL
---TELEPORT_SKILL
 ConsoleService:ExecuteCommand('bind key_20 "activate_slot_request JUMP_SKILL"')
 ConsoleService:ExecuteCommand('bind #20 "activate_slot_request JUMP_SKILL"')
 ConsoleService:ExecuteCommand('bind 20 "activate_slot_request JUMP_SKILL"')
+ConsoleService:ExecuteCommand('bind capslock "activate_slot_request JUMP_SKILL"')
+ConsoleService:ExecuteCommand('bind caps "activate_slot_request JUMP_SKILL"')
+ConsoleService:ExecuteCommand('bind KEY_CAPSLOCK "activate_slot_request JUMP_SKILL"')
+ConsoleService:ExecuteCommand('bind KEY_CAPS "activate_slot_request JUMP_SKILL"')
+
+--DODGE_ROLL_SKILL
 ConsoleService:ExecuteCommand('bind lcontrol "activate_slot_request DODGE_ROLL_SKILL"')
+
+--TELEPORT_SKILL
 ConsoleService:ExecuteCommand('bind lalt "activate_slot_request TELEPORT_SKILL"')
 
 ConsoleService:ExecuteCommand('bind 9 "activate_slot_request USABLE_9"')
+
 ConsoleService:ExecuteCommand('bind 0 "activate_slot_request USABLE_10"')
+
+ConsoleService:ExecuteCommand('bind "-" "activate_slot_request USABLE_11"')
+
+ConsoleService:ExecuteCommand('bind "=" "activate_slot_request USABLE_12"')
