12c34934811b251acd1aa5c6457a09f91a813f65
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-03-09 21:13:00 +0300

    TestMod. QuickEquipmentSlotsUtils.

--- TestMod/lua/quick_equipment_slots_change_autoexec.lua
+++ TestMod/lua/quick_equipment_slots_change_autoexec.lua
@@ -1,205 +1,196 @@
+local QuickEquipmentSlotsUtils = require("lua/utils/quick_equipment_slots_utils.lua")
 require("lua/utils/reflection.lua")
 require("lua/utils/table_utils.lua")
 
--- save_equipment upgrade upgrade_standart
--- save_equipment upgrade upgrade_standart2
+-- save_eq_upgrade standart
+-- save_eq_upgrade standart2
 
--- save_equipment usable usable_standart
--- save_equipment left_hand left_hand_standart
--- save_equipment right_hand right_hand_standart
+-- save_eq_usable standart
+-- save_eq_left standart
+-- save_eq_right standart
 
-ConsoleService:RegisterCommand( "save_equipment", function( args )
+-- load_eq_upgrade standart
+-- load_eq_upgrade standart2
 
-    if not Assert( #args == 2, "Command save_equipment requires two arguments! [slot_name_prefix] [configname] " .. tostring(#args) ) then
-        return 
-    end
+-- load_eq_usable standart
+-- load_eq_left standart
+-- load_eq_right standart
 
-    local slotNamePrefix = args[1]
-    local configName = args[2]
+ConsoleService:RegisterCommand( "save_eq_upgrade", function( args )
 
-    LogService:Log("save_equipment " .. slotNamePrefix .. " " .. configName )
-    
-    local player = PlayerService:GetPlayerControlledEnt(0)
-    if player == INVALID_ID then
-        return
-    end
-
-    local campaignDatabase = CampaignService:GetCampaignData()
-    if ( campaignDatabase == nil ) then
-        return
-    end
-
-    local equipment = reflection_helper( EntityService:GetComponent(player, "EquipmentComponent") ).equipment[1]
-    if equipment.id then
-        equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
-    end
-
-    if ( equipment == nil ) then
-        return
-    end
-
-    slotNamePrefix = string.lower(slotNamePrefix)
-
-    local configContent = ""
-
-    local slots = equipment.slots
-
-    for i=1,slots.count do
-
-        local slot = slots[i]
-
-        local slotName = string.lower(slot.name)
-
-        if ( string.find( slotName, slotNamePrefix ) ~= nil ) then
-
-            local subSlotsConfig = ""
-
-            for j=1,slot.subslots.count do
-
-                local entities = slot.subslots[j]
-
-                local entity = entities[1]
-
-                if (entity) then
-
-                    if entity.id then
-                        entity = entity.id
-                    end
-
-                    if ( string.len( subSlotsConfig ) > 0 ) then
-                        subSlotsConfig = subSlotsConfig .. ";"
-                    end
-
-                    subSlotsConfig = subSlotsConfig .. tostring(j) .. "," .. tostring(entity)
-                end
-            end
-
-            if ( string.len( subSlotsConfig ) > 0 ) then
-
-                if ( string.len( configContent ) > 0 ) then
-                    configContent = configContent .. "|"
-                end
-
-                configContent = configContent .. slot.name .. ":" .. subSlotsConfig
-            end            
-        end
-    end
-
-    local keyName = "$save_equipment." .. configName
-
-    LogService:Log("save_equipment key " .. keyName .. " configContent " .. configContent )
-
-    campaignDatabase:SetString( keyName, configContent )
-    
-end)
-
--- load_equipment upgrade upgrade_standart
--- load_equipment usable usable_standart
--- load_equipment left_hand left_hand_standart
--- load_equipment right_hand right_hand_standart
-
-ConsoleService:RegisterCommand( "load_equipment", function( args )
-
-    if not Assert( #args == 1, "Command load_equipment requires one arguments! [configname] " .. tostring(#args) ) then
+    if not Assert( #args == 1, "Command save_eq_upgrade requires one arguments! [configname] " .. tostring(#args) ) then
         return 
     end
 
     local configName = args[1]
 
-    LogService:Log("load_equipment " .. configName )
+    LogService:Log("save_eq_upgrade " .. configName )
+
+    QuickEquipmentSlotsUtils:SaveEquipment( "upgrade", configName )
+end)
+
+ConsoleService:RegisterCommand( "save_eq_usable", function( args )
+
+    if not Assert( #args == 1, "Command save_eq_usable requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("save_eq_usable " .. configName )
+
+    QuickEquipmentSlotsUtils:SaveEquipment( "usable", configName )
+end)
+
+ConsoleService:RegisterCommand( "save_eq_left", function( args )
+
+    if not Assert( #args == 1, "Command save_eq_left requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("save_eq_left " .. configName )
+
+    QuickEquipmentSlotsUtils:SaveEquipment( "left_hand", configName )
+end)
+
+ConsoleService:RegisterCommand( "save_eq_right", function( args )
+
+    if not Assert( #args == 1, "Command save_eq_right requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("save_eq_right " .. configName )
+
+    QuickEquipmentSlotsUtils:SaveEquipment( "right_hand", configName )
+end)
+
+ConsoleService:RegisterCommand( "save_eq_weapon", function( args )
+
+    if not Assert( #args == 1, "Command save_eq_weapon requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("save_eq_weapon " .. configName )
     
-    local player = PlayerService:GetPlayerControlledEnt(0)
-    if player == INVALID_ID then
-        return
+    QuickEquipmentSlotsUtils:SaveEquipment( "left_hand", configName )
+    QuickEquipmentSlotsUtils:SaveEquipment( "right_hand", configName )
+end)
+
+ConsoleService:RegisterCommand( "save_eq_all", function( args )
+
+    if not Assert( #args == 1, "Command save_eq_all requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
     end
 
-    local campaignDatabase = CampaignService:GetCampaignData()
-    if ( campaignDatabase == nil ) then
-        return
-    end
+    local configName = args[1]
+
+    LogService:Log("save_eq_all " .. configName )
     
-    local equipment = reflection_helper( EntityService:GetComponent(player, "EquipmentComponent") ).equipment[1]
-    if equipment.id then
-        equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
+    QuickEquipmentSlotsUtils:SaveEquipment( "upgrade", configName )
+    QuickEquipmentSlotsUtils:SaveEquipment( "usable", configName )
+
+    QuickEquipmentSlotsUtils:SaveEquipment( "left_hand", configName )
+    QuickEquipmentSlotsUtils:SaveEquipment( "right_hand", configName )
+end)
+
+
+
+
+
+
+
+
+ConsoleService:RegisterCommand( "load_eq_upgrade", function( args )
+
+    if not Assert( #args == 1, "Command load_eq_upgrade requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
     end
 
-    if ( equipment == nil ) then
-        return
+    local configName = args[1]
+
+    LogService:Log("load_eq_upgrade " .. configName )
+
+    QuickEquipmentSlotsUtils:LoadEquipment( "upgrade", configName )
+
+end)
+
+ConsoleService:RegisterCommand( "load_eq_usable", function( args )
+
+    if not Assert( #args == 1, "Command load_eq_usable requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
     end
 
-    local slots = equipment.slots
+    local configName = args[1]
 
-    local keyName = "$save_equipment." .. configName
+    LogService:Log("load_eq_usable " .. configName )
 
-    local configContent = campaignDatabase:GetStringOrDefault( keyName, "" )
+    QuickEquipmentSlotsUtils:LoadEquipment( "usable", configName )
 
-    LogService:Log("load_equipment key " .. keyName .. " configContent " .. configContent )
+end)
 
-    local configContentArray = Split( configContent, "|" )
+ConsoleService:RegisterCommand( "load_eq_left", function( args )
 
-    for template in Iter( configContentArray ) do
-
-        local slotValuesArray = Split( template, ":" )
-
-        if ( #slotValuesArray ~= 2 ) then
-            goto continue
-        end
-
-        local slotName = slotValuesArray[1]
-
-        local subSlotsConfig = slotValuesArray[2]
-
-        local slotExists = false
-
-        local selectedSlot = nil
-
-        for i=1,slots.count do
-
-            selectedSlot = slots[i]
-
-            if selectedSlot.name == slotName then
-                slotExists = true
-                break
-            end
-        end
-                
-        if ( not slotExists ) then
-            goto continue
-        end
-
-        -- Split array of coordinates by ";"
-        local subSlotsConfigArray = Split( subSlotsConfig, ";" )
-
-        for subSlotString in Iter( subSlotsConfigArray ) do
-
-            local subSlotStringArray = Split( subSlotString, "," )
-
-            if ( #subSlotStringArray == 2 ) then
-
-                local subSlotNumber = tonumber(subSlotStringArray[1])
-                local subSlotEntityId = tonumber(subSlotStringArray[2])
-
-                if ( subSlotNumber ~= nil and subSlotEntityId ~= nil ) then
-
-                    if ( 1 <= subSlotNumber and subSlotNumber <= selectedSlot.subslots_count ) then
-
-                        local blueprintName = EntityService:GetBlueprintName( subSlotEntityId )
-
-                        LogService:Log("#blueprintName subSlotEntityId " .. tostring(subSlotEntityId) .. " blueprintName " .. tostring(blueprintName) )
-
-                        if ( blueprintName ~= nil and blueprintName ~= "") then
-
-                            LogService:Log("TryEquipItemInSlot slotName " .. slotName .. " subSlotNumber " .. tostring(subSlotNumber) .. " subSlotEntityId " .. tostring(subSlotEntityId) )
-
-                            ItemService:TryEquipItemInSlot( player, subSlotEntityId, slotName, subSlotNumber - 1 )
-                        else
-                            LogService:Log("#blueprintName == nil " )
-                        end
-                    end
-                end
-            end
-        end
-
-        ::continue::
+    if not Assert( #args == 1, "Command load_eq_left requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
     end
+
+    local configName = args[1]
+
+    LogService:Log("load_eq_left " .. configName )
+
+    QuickEquipmentSlotsUtils:LoadEquipment( "left_hand", configName )
+
+end)
+
+ConsoleService:RegisterCommand( "load_eq_right", function( args )
+
+    if not Assert( #args == 1, "Command load_eq_right requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("load_eq_right " .. configName )
+
+    QuickEquipmentSlotsUtils:LoadEquipment( "right_hand", configName )
+
+end)
+
+ConsoleService:RegisterCommand( "load_eq_weapon", function( args )
+
+    if not Assert( #args == 1, "Command load_eq_weapon requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("load_eq_weapon " .. configName )
+    
+    QuickEquipmentSlotsUtils:LoadEquipment( "left_hand", configName )
+    QuickEquipmentSlotsUtils:LoadEquipment( "right_hand", configName )
+
+end)
+
+ConsoleService:RegisterCommand( "load_eq_all", function( args )
+
+    if not Assert( #args == 1, "Command load_eq_all requires one arguments! [configname] " .. tostring(#args) ) then
+        return 
+    end
+
+    local configName = args[1]
+
+    LogService:Log("load_eq_all " .. configName )
+    
+    QuickEquipmentSlotsUtils:LoadEquipment( "upgrade", configName )
+    QuickEquipmentSlotsUtils:LoadEquipment( "usable", configName )
+
+    QuickEquipmentSlotsUtils:LoadEquipment( "left_hand", configName )
+    QuickEquipmentSlotsUtils:LoadEquipment( "right_hand", configName )
+
 end)
---
+++ TestMod/lua/utils/quick_equipment_slots_utils.lua
@@ -0,0 +1,209 @@
+require("lua/utils/reflection.lua")
+require("lua/utils/table_utils.lua")
+require("lua/utils/string_utils.lua")
+
+local QuickEquipmentSlotsUtils = {}
+
+function QuickEquipmentSlotsUtils:SaveEquipment( slotNamePrefix, configName )
+
+    local player = PlayerService:GetPlayerControlledEnt(0)
+    if player == INVALID_ID then
+        return
+    end
+
+    local campaignDatabase = CampaignService:GetCampaignData()
+    if ( campaignDatabase == nil ) then
+        return
+    end
+
+    local equipment = reflection_helper( EntityService:GetComponent(player, "EquipmentComponent") ).equipment[1]
+    if equipment.id then
+        equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
+    end
+
+    if ( equipment == nil ) then
+        return
+    end
+
+    --LogService:Log( "QuickEquipmentSlotsUtils equipment " .. tostring(equipment) )
+
+    slotNamePrefix = string.lower(slotNamePrefix)
+
+    local configContent = ""
+
+    local slots = equipment.slots
+
+    for i=1,slots.count do
+
+        local slot = slots[i]
+
+        local slotName = string.lower(slot.name)
+
+        if ( string.find( slotName, slotNamePrefix ) ~= nil ) then
+
+            local subSlotsConfig = ""
+
+            for j=1,slot.subslots.count do
+
+                local entities = slot.subslots[j]
+
+                local entity = entities[1]
+
+                if (entity) then
+
+                    if entity.id then
+                        entity = entity.id
+                    end
+
+                    if ( string.len( subSlotsConfig ) > 0 ) then
+                        subSlotsConfig = subSlotsConfig .. ";"
+                    end
+
+                    subSlotsConfig = subSlotsConfig .. tostring(j) .. "," .. tostring(entity)
+                end
+            end
+
+            if ( string.len( subSlotsConfig ) > 0 ) then
+
+                if ( string.len( configContent ) > 0 ) then
+                    configContent = configContent .. "|"
+                end
+
+                configContent = configContent .. slot.name .. ":" .. subSlotsConfig
+            end            
+        end
+    end
+
+    local keyName = QuickEquipmentSlotsUtils:GetSettingKeyName( slotNamePrefix, configName )
+
+    LogService:Log("save_equipment key " .. keyName .. " configContent " .. configContent )
+
+    campaignDatabase:SetString( keyName, configContent )
+end
+
+function QuickEquipmentSlotsUtils:GetSettingKeyName( slotNamePrefix, configName )
+
+    local keyName = "$QuickEquipmentSlotsUtils." .. slotNamePrefix .. "." .. configName
+
+    return keyName
+end
+
+function QuickEquipmentSlotsUtils:LoadEquipment( slotNamePrefix, configName )
+    
+    local player = PlayerService:GetPlayerControlledEnt(0)
+    if player == INVALID_ID then
+        return
+    end
+
+    local campaignDatabase = CampaignService:GetCampaignData()
+    if ( campaignDatabase == nil ) then
+        return
+    end
+    
+    local equipment = reflection_helper( EntityService:GetComponent(player, "EquipmentComponent") ).equipment[1]
+    if equipment.id then
+        equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
+    end
+
+    if ( equipment == nil ) then
+        return
+    end
+
+    local slots = equipment.slots
+
+    local keyName = QuickEquipmentSlotsUtils:GetSettingKeyName( slotNamePrefix, configName )
+
+    local configContent = campaignDatabase:GetStringOrDefault( keyName, "" )
+
+    configContent = configContent or ""
+
+    LogService:Log("load_equipment key " .. keyName .. " configContent " .. configContent )
+
+    if ( configContent == "" ) then
+
+        return
+    end
+
+    local configContentArray = Split( configContent, "|" )
+
+    for template in Iter( configContentArray ) do
+
+        local slotValuesArray = Split( template, ":" )
+
+        if ( #slotValuesArray ~= 2 ) then
+            goto continue
+        end
+
+        local slotName = slotValuesArray[1]
+
+        local subSlotsConfig = slotValuesArray[2]
+
+        if ( string.find( slotName, slotNamePrefix ) ~= nil ) then
+            goto continue
+        end
+
+        local slotExists = false
+
+        local selectedSlot = nil
+
+        for i=1,slots.count do
+
+            selectedSlot = slots[i]
+
+            if selectedSlot.name == slotName then
+                slotExists = true
+                break
+            end
+        end
+                
+        if ( not slotExists ) then
+            goto continue
+        end
+
+        -- Split array of coordinates by ";"
+        local subSlotsConfigArray = Split( subSlotsConfig, ";" )
+
+        for subSlotString in Iter( subSlotsConfigArray ) do
+
+            QuickEquipmentSlotsUtils:LoadEquipmentToSlot( player, slotName, selectedSlot.subslots_count, subSlotString )
+        end
+
+        ::continue::
+    end
+end
+
+function QuickEquipmentSlotsUtils:LoadEquipmentToSlot( player, slotName, subslots_count, subSlotString )
+
+    local subSlotStringArray = Split( subSlotString, "," )
+
+    if ( #subSlotStringArray ~= 2 ) then
+        return
+    end
+
+    local subSlotNumber = tonumber(subSlotStringArray[1])
+    local subSlotEntityId = tonumber(subSlotStringArray[2])
+
+    if ( subSlotNumber == nil or subSlotEntityId == nil ) then
+        return
+    end
+
+    if ( subslots_count < subSlotNumber or subSlotNumber <= 0 ) then
+        return
+    end
+
+    local blueprintName = EntityService:GetBlueprintName( subSlotEntityId )
+    blueprintName = blueprintName or ""
+
+    LogService:Log("#blueprintName subSlotEntityId " .. tostring(subSlotEntityId) .. " blueprintName " .. tostring(blueprintName) )
+
+    if ( blueprintName == "") then
+        LogService:Log("#blueprintName == nil " )
+        return
+    end
+
+    LogService:Log("TryEquipItemInSlot slotName " .. slotName .. " subSlotNumber " .. tostring(subSlotNumber) .. " subSlotEntityId " .. tostring(subSlotEntityId) )
+
+    ItemService:TryEquipItemInSlot( player, subSlotEntityId, slotName, subSlotNumber - 1 )
+end
+
+return QuickEquipmentSlotsUtils;
