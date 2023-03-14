1c8312c815a061de36333db758f207c717fd1a2f
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-03-14 22:53:06 +0300

    TowerModsTools. Hide tool.

--- TowerModsTools/lua/tower_mods_tools_autoexec.lua
+++ TowerModsTools/lua/tower_mods_tools_autoexec.lua
@@ -1,6 +1,6 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
     
-    BuildingService:UnlockBuilding("buildings/tools/tower_mods_remover")
+    --BuildingService:UnlockBuilding("buildings/tools/tower_mods_remover")
     --BuildingService:UnlockBuilding("buildings/tools/sapling_replacer")
 end)
 
