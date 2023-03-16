f809897d4ab5a50040cd2b7c2196f4814976a95e
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-03-16 10:13:45 +0300

    TestMod. Moving files to UnlockCustomResearchesInSaves.

--- TestMod/lua/researches_unlock_custom_trees_autoexec.lua
+++
@@ -1,7 +0,0 @@
-local Research = require("lua/utils/researches_unlock_custom_trees_utils.lua")
-
-RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
-
-    Research:CheckResearches( )
-end)
-
