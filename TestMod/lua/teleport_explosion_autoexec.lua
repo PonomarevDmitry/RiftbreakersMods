38eea409dd8e51055efde56de8424860b35d3cb6
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-03-26 02:01:12 +0300

    ElementalTeleportsSkills. Moving file to TestMod.

---
+++ TestMod/lua/teleport_explosion_autoexec.lua
@@ -0,0 +1,81 @@
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_acid_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_acid_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_acid_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_acid_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_energy_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_energy_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_energy_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_energy_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_fire_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_fire_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_fire_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_fire_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_ice_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_ice_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_ice_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_attack_ice_extreme_item", false)
+
+
+
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_acid_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_acid_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_acid_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_acid_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_energy_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_energy_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_energy_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_energy_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_fire_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_fire_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_fire_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_fire_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_ice_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_ice_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_ice_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_emergency_ice_extreme_item", false)
+
+
+
+    PlayerService:AddGlobalAward("items/skills/teleport_double_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_double_acid_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_acid_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_acid_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_acid_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_double_energy_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_energy_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_energy_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_energy_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_double_fire_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_fire_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_fire_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_fire_extreme_item", false)
+
+    PlayerService:AddGlobalAward("items/skills/teleport_double_ice_standard_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_ice_advanced_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_ice_superior_item", false)
+    PlayerService:AddGlobalAward("items/skills/teleport_double_ice_extreme_item", false)
+end)
