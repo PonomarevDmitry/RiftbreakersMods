dfd9c077c684a604da3fa6f51bf96198de905d5a
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-04-05 23:39:51 +0300

    TestMod. Commenting attack floors unlocking.

--- TestMod/lua/attack_floors_autoexec.lua
+++ TestMod/lua/attack_floors_autoexec.lua
@@ -1,33 +1,33 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
     
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_4x4")
-
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_4x4")
-
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_4x4")
-
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_4x4")
-
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_4x4")
-
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_1x1")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_2x2")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_3x3")
-    BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_4x4")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_acid_4x4")
+    --
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_cryo_4x4")
+    --
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_energy_4x4")
+    --
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_fire_4x4")
+    --
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_area_4x4")
+    --
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_1x1")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_2x2")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_3x3")
+    --BuildingService:UnlockBuilding("buildings/decorations/attack_floor_physical_4x4")
 end)
 
