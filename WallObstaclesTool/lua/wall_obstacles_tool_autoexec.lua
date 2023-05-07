308304993e3608da5bc3d262713812a511a37b0a
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-07 14:07:05 +0300

    WallObstaclesTool. wall_obstacles_stairs_tool.

--- WallObstaclesTool/lua/wall_obstacles_tool_autoexec.lua
+++ WallObstaclesTool/lua/wall_obstacles_tool_autoexec.lua
@@ -1,6 +1,7 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
-    
+
     BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_tool")
+    BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_stairs_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_area_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_perimeter_tool")
 end)
