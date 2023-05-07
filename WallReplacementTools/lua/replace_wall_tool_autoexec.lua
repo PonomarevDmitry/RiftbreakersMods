0608b3d471d769827ca063b1d606462cfb811482
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-07 12:58:30 +0300

    WallReplacementTools. Adding crystal walls replacement tool.

--- WallReplacementTools/lua/replace_wall_tool_autoexec.lua
+++ WallReplacementTools/lua/replace_wall_tool_autoexec.lua
@@ -1,5 +1,6 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
-    
+
+    BuildingService:UnlockBuilding("buildings/tools/replace_wall_to_crystal")
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_to_energy")
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_to_small")
 end)
