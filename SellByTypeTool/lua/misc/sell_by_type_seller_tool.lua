5fbc529e8aee57dbf522e252674165f16bc1b592
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-07 11:12:57 +0300

    SellByTypeTool. Refactoring.

--- SellByTypeTool/lua/misc/sell_by_type_seller_tool.lua
+++ SellByTypeTool/lua/misc/sell_by_type_seller_tool.lua
@@ -16,6 +16,9 @@ function sell_by_type_seller_tool:OnInit()
     self.isGroup = (self.data:GetIntOrDefault("is_group", 0) == 1)
 
     self.placeRuins = (self.data:GetIntOrDefault("place_ruins", 0) == 1)
+
+    self.previousMarkedBuildings = {}
+    self.radiusShowBuildings = 100.0
 end
 
 function sell_by_type_seller_tool:GetScaleFromDatabase()
@@ -48,35 +51,9 @@ function sell_by_type_seller_tool:FilterSelectedEntities( selectedEntities )
 
     for entity in Iter( selectedEntities ) do
 
-        local blueprintName = EntityService:GetBlueprintName(entity)
-
-        local buildingComponent = EntityService:GetComponent( entity, "BuildingComponent" )
-        if ( buildingComponent == nil ) then
+        if ( not self:IsEntityApproved(entity) ) then
             goto continue
         end
-
-        local buildingComponentRef = reflection_helper(buildingComponent)
-        if ( buildingComponentRef.m_isSellable == false ) then
-            goto continue
-        end
-
-        local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
-        if ( buildingDesc == nil ) then
-            goto continue
-        end
-
-        if ( self.isGroup ) then
-
-            if ( not self:IsBlueprintInLowNameList(blueprintName) ) then
-                goto continue
-            end
-        else
-
-            if ( not self:IsBlueprintInList(blueprintName) ) then
-                goto continue
-            end
-        end
-
 
         Insert(entities, entity)
 
@@ -86,7 +63,66 @@ function sell_by_type_seller_tool:FilterSelectedEntities( selectedEntities )
     return entities
 end
 
+function sell_by_type_seller_tool:IsEntityApproved( entity )
+
+    local buildingComponent = EntityService:GetComponent( entity, "BuildingComponent" )
+    if ( buildingComponent == nil ) then
+        return false
+    end
+
+    local buildingComponentRef = reflection_helper(buildingComponent)
+    if ( buildingComponentRef.m_isSellable == false ) then
+        return false
+    end
+
+    local blueprintName = EntityService:GetBlueprintName(entity)
+
+    local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
+    if ( buildingDesc == nil ) then
+        return false
+    end
+
+    if ( self.isGroup ) then
+
+        if ( not self:IsBlueprintInLowNameList(blueprintName) ) then
+            return false
+        end
+    else
+
+        if ( not self:IsBlueprintInList(blueprintName) ) then
+            return false
+        end
+    end
+
+    return true
+end
+
 function sell_by_type_seller_tool:OnUpdate()
+
+    local sellableBuildinsList = self:FindBuildingSellable()
+    
+    self.previousMarkedBuildings = self.previousMarkedBuildings or {}
+    
+    for entity in Iter( self.previousMarkedBuildings ) do
+    
+        -- If the building is not included in the new list
+        if ( IndexOf( sellableBuildinsList, entity ) == nil and IndexOf( self.selectedEntities, entity ) == nil ) then
+            self:RemovedFromSelection( entity )
+        end
+    end
+    
+    for entity in Iter( sellableBuildinsList ) do
+        
+        local skinned = EntityService:IsSkinned( entity )
+        if ( skinned ) then
+            EntityService:SetMaterial( entity, "selector/hologram_skinned_pass", "selected")
+        else
+            EntityService:SetMaterial( entity, "selector/hologram_pass", "selected")
+        end
+    end
+    
+    self.previousMarkedBuildings = sellableBuildinsList
+
 
     self.sellCosts = {}
     
@@ -114,8 +150,48 @@ function sell_by_type_seller_tool:OnUpdate()
     end
 end
 
-function sell_by_type_seller_tool:OnRotate()
+function sell_by_type_seller_tool:FindBuildingSellable()
 
+    local player = PlayerService:GetPlayerControlledEnt(0)
+
+    local buildings = FindService:FindEntitiesByTypeInRadius( player, "building", self.radiusShowBuildings )
+    
+    local result = {}
+    
+    for entity in Iter( buildings ) do
+
+        if ( IndexOf( self.selectedEntities, entity ) ~= nil ) then
+            goto continue
+        end
+
+        if ( not self:IsEntityApproved(entity) ) then
+            goto continue
+        end
+
+        Insert( result, entity )
+
+        ::continue::
+    end
+    
+    return result
+end
+
+function sell_by_type_seller_tool:OnRotate()
+end
+
+function sell_by_type_seller_tool:OnRelease()
+    
+    if ( self.previousMarkedBuildings ~= nil) then
+        for ent in Iter( self.previousMarkedBuildings ) do
+            self:RemovedFromSelection( ent )
+        end
+    end
+    self.previousMarkedBuildings = {}
+
+    if ( sell_by_type_base.OnRelease ) then
+
+        sell_by_type_base.OnRelease(self)
+    end
 end
 
 function sell_by_type_seller_tool:OnActivateEntity( entity )
