0317787c27e2117503dfc6325384edc7e3621da2
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-08 08:59:44 +0300

    WallObstaclesTools. Changing wall_perimeter_tool to wall_borders_tool.

--- WallObstaclesTools/lua/wall_obstacles_tool_autoexec.lua
+++ WallObstaclesTools/lua/wall_obstacles_tool_autoexec.lua
@@ -1,7 +1,8 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
+    BuildingService:UnlockBuilding("buildings/defense/wall_area_tool")
+    BuildingService:UnlockBuilding("buildings/defense/wall_borders_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_stairs_tool")
-    BuildingService:UnlockBuilding("buildings/defense/wall_area_tool")
-    BuildingService:UnlockBuilding("buildings/defense/wall_perimeter_tool")
+
 end)
