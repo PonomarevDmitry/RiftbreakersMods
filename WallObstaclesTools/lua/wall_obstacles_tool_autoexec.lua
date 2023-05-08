9841102ec21d638575af58cd9451611cc70ffcf3
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-08 18:12:17 +0300

    WallObstaclesTools. Adding Diagonal Wall Tool.

--- WallObstaclesTools/lua/wall_obstacles_tool_autoexec.lua
+++ WallObstaclesTools/lua/wall_obstacles_tool_autoexec.lua
@@ -2,6 +2,7 @@ RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/defense/wall_area_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_borders_tool")
+    BuildingService:UnlockBuilding("buildings/defense/wall_diagonal_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_tool")
     BuildingService:UnlockBuilding("buildings/defense/wall_obstacles_stairs_tool")
 
