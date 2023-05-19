5339081df07bd7a35d286a4bb49d04273f1e16af
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-19 22:39:01 +0300

    WallReplacementTools. Renaming tools picker 1 and 2.

--- WallReplacementTools/lua/replace_wall_tool_autoexec.lua
+++ WallReplacementTools/lua/replace_wall_tool_autoexec.lua
@@ -4,10 +4,12 @@ RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_to_energy")
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_to_small")
 
-    BuildingService:UnlockBuilding("buildings/tools/replace_wall_from_to_picker_1")
-    BuildingService:UnlockBuilding("buildings/tools/replace_wall_from_to_picker_2")
+    BuildingService:UnlockBuilding("buildings/tools/replace_wall_all_picker")
+    BuildingService:UnlockBuilding("buildings/tools/replace_wall_all_replacer")
+
+    BuildingService:UnlockBuilding("buildings/tools/replace_wall_picker_1")
+    BuildingService:UnlockBuilding("buildings/tools/replace_wall_picker_2")
 
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_from_to_replacer_1_2")
     BuildingService:UnlockBuilding("buildings/tools/replace_wall_from_to_replacer_2_1")
 end)
-
