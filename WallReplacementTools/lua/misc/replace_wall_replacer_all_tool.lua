49ad5812774cc5984ab4c26d68a0efa86c47a8e0
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-20 17:39:27 +0300

    WallReplacementTools. Remove highlighting.

--- WallReplacementTools/lua/misc/replace_wall_replacer_all_tool.lua
+++ WallReplacementTools/lua/misc/replace_wall_replacer_all_tool.lua
@@ -40,9 +40,6 @@ function replace_wall_replacer_all_tool:OnInit()
     end
 
     self:SetBuildingIcon()
-
-    self.previousMarkedBuildings = {}
-    self.radiusShowBuildings = 100.0
 end
 
 function replace_wall_replacer_all_tool:FillAllBlueprints( blueprintName )
@@ -218,31 +215,6 @@ end
 
 function replace_wall_replacer_all_tool:OnUpdate()
 
-    local buildinsList = self:FindBuildingFrom()
-
-    self.previousMarkedBuildings = self.previousMarkedBuildings or {}
-
-    for entity in Iter( self.previousMarkedBuildings ) do
-
-        if ( IndexOf( buildinsList, entity ) == nil and IndexOf( self.selectedEntities, entity ) == nil ) then
-            self:RemovedFromSelection( entity )
-        end
-    end
-
-    for entity in Iter( buildinsList ) do
-
-        local skinned = EntityService:IsSkinned( entity )
-        if ( skinned ) then
-            EntityService:SetMaterial( entity, "selector/hologram_active_skinned", "selected" )
-        else
-            EntityService:SetMaterial( entity, "selector/hologram_active", "selected" )
-        end
-    end
-
-    self.previousMarkedBuildings = buildinsList
-
-
-
     local costResourceList = {}
     local costValues = {}
 
@@ -312,47 +284,7 @@ function replace_wall_replacer_all_tool:OnUpdate()
     end
 end
 
-function replace_wall_replacer_all_tool:FindBuildingFrom()
-
-    local player = PlayerService:GetPlayerControlledEnt(self.playerId)
-
-    local buildings = FindService:FindEntitiesByTypeInRadius( player, "building", self.radiusShowBuildings )
-
-    local result = {}
-
-    for entity in Iter( buildings ) do
-
-        if ( IndexOf( self.selectedEntities, entity ) ~= nil ) then
-            goto continue
-        end
-
-        if ( not self:IsEntityApproved(entity) ) then
-            goto continue
-        end
-
-        Insert( result, entity )
-
-        ::continue::
-    end
-
-    return result
-end
-
 function replace_wall_replacer_all_tool:OnRotate()
-end
-
-function replace_wall_replacer_all_tool:OnRelease()
-
-    if ( self.previousMarkedBuildings ~= nil) then
-        for ent in Iter( self.previousMarkedBuildings ) do
-            self:RemovedFromSelection( ent )
-        end
-    end
-    self.previousMarkedBuildings = {}
-
-    if ( replace_wall_base.OnRelease ) then
-        replace_wall_base.OnRelease(self)
-    end
 end
 
 function replace_wall_replacer_all_tool:OnActivateEntity( entity )
