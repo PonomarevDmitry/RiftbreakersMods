d98b608c65c6c022ec34444fcfb4b8d8a04f6dc4
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-23 22:20:35 +0300

    PersonalRiftPortalSkill. Immobilize mech while spawning personal portal.

--- PersonalRiftPortalSkill/lua/items/skills/personal_rift_portal.lua
+++ PersonalRiftPortalSkill/lua/items/skills/personal_rift_portal.lua
@@ -17,43 +17,53 @@ function personal_rift_portal:OnActivate()
 
     local database = EntityService:GetBlueprintDatabase( self.entity ) or self.data
 
-    self.spawnWaiting = database:GetFloat("spawn_waiting")
-    self.spawnDuration = database:GetFloat("spawn_duration")
+    self.teleportBefore = database:GetFloat("teleport_before")
+    self.spawnAfter = database:GetFloat("spawn_after")
 
-    if ( self.spawnWaiting <= self.duration and self.duration <= self.spawnDuration ) then
+    if ( self.teleportBefore <= self.duration and self.duration <= self.spawnAfter ) then
 
         if ( self.timer == nil ) then
 
-            local timeBeforeSpawn = (self.spawnDuration - self.spawnWaiting)
+            QueueEvent( "AddMaxSpeedModifierRequest", self.owner, "personal_rift_portal_penalty", 0 )
+
+            local timeBeforeSpawn = (self.spawnAfter - self.teleportBefore)
 
             self.timer = BuildingService:CreateGuiTimer( self.owner, timeBeforeSpawn )
         else
 
-            local passTime = (self.duration - self.spawnWaiting)
+            local passTime = (self.duration - self.teleportBefore)
 
             BuildingService:SetGuiTimer( self.timer, passTime )
         end
-    elseif ( self.spawnDuration <= self.duration ) then
+    elseif ( self.spawnAfter < self.duration ) then
 
         if ( self.timer ~= nil ) then
-            EntityService:RemoveEntity( self.timer )
-            self.timer = nil
-        end
 
-        self:SpawnPortal( "misc/personal_rift" )
+            self:DestroyTimer()
+
+            self:SpawnPortal( "misc/personal_rift" )
+        end
     end
 
     self.duration = self.duration + 1.0 / 30.0
 end
 
-function personal_rift_portal:OnDeactivate()
+function personal_rift_portal:DestroyTimer()
 
     if ( self.timer ~= nil ) then
+
         EntityService:RemoveEntity( self.timer )
         self.timer = nil
-    end
 
-    if ( self.duration <= self.spawnWaiting ) then
+        QueueEvent( "RemoveMaxSpeedModifierRequest", self.owner, "personal_rift_portal_penalty" )
+    end
+end
+
+function personal_rift_portal:OnDeactivate()
+
+    self:DestroyTimer()
+
+    if ( self.duration < self.teleportBefore ) then
 
         local entities = FindService:FindEntitiesByBlueprint( "misc/personal_rift" )
 
