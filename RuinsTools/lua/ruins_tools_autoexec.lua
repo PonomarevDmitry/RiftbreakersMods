a0135fa57880b7fbd30c8b3b18f8136627cfb52f
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-05-27 15:56:16 +0300

    RuinsTools. Rename tools.

--- RuinsTools/lua/ruins_tools_autoexec.lua
+++ RuinsTools/lua/ruins_tools_autoexec.lua
@@ -1,5 +1,5 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
-    BuildingService:UnlockBuilding("buildings/tools/sell_place_ruins")
-    BuildingService:UnlockBuilding("buildings/tools/sell_ruins_eraser")
+    BuildingService:UnlockBuilding("buildings/tools/sell_1_place_ruins")
+    BuildingService:UnlockBuilding("buildings/tools/sell_1_ruins_eraser")
 end)
