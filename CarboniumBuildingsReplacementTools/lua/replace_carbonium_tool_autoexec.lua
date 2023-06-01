b78235452ef781b0f9bba2477aba752b00487699
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-06-01 20:37:25 +0300

    CarboniumBuildingsReplacementTools. Removing logging.

--- CarboniumBuildingsReplacementTools/lua/replace_carbonium_tool_autoexec.lua
+++ CarboniumBuildingsReplacementTools/lua/replace_carbonium_tool_autoexec.lua
@@ -1,7 +1,5 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
-    LogService:Log("Unlocking")
-
     BuildingService:UnlockBuilding("buildings/tools/replace_carbonium_to_1_powerplant")
     BuildingService:UnlockBuilding("buildings/tools/replace_carbonium_to_2_factory")
 end)
