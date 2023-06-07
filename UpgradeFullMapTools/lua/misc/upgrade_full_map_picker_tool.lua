c16c43fd1aa15571030b2ad1da449cf342ae6946
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2023-06-07 22:32:04 +0300

    UpgradeFullMapTools. Highlight upgradable buildings.

--- UpgradeFullMapTools/lua/misc/upgrade_full_map_picker_tool.lua
+++ UpgradeFullMapTools/lua/misc/upgrade_full_map_picker_tool.lua
@@ -50,6 +50,11 @@ function upgrade_full_map_picker_tool:OnInit()
     else
         markerDB:SetInt("building_visible", 0)
     end
+
+    -- List of buildings highlighted for upgrade
+    self.previousMarkedBuildings = {}
+    -- Radius from player to highlight buildings for upgrade
+    self.radiusShowBuildingsToUpgrade = 100.0
 end
 
 function upgrade_full_map_picker_tool:SpawnCornerBlueprint()
@@ -62,9 +67,9 @@ function upgrade_full_map_picker_tool:AddedToSelection( entity )
 
     local skinned = EntityService:IsSkinned(entity)
     if ( skinned ) then
-        EntityService:SetMaterial( entity, "selector/hologram_current_skinned", "selected")
+        EntityService:SetMaterial( entity, "selector/hologram_current_skinned", "selected" )
     else
-        EntityService:SetMaterial( entity, "selector/hologram_current", "selected")
+        EntityService:SetMaterial( entity, "selector/hologram_current", "selected" )
     end
 end
 
@@ -115,6 +120,11 @@ function upgrade_full_map_picker_tool:FilterSelectedEntities( selectedEntities )
     return entities
 end
 
+function upgrade_tool:OnUpdate()
+
+    self:HighlightBuildingsToUpgrade()
+end
+
 function upgrade_full_map_picker_tool:IsUpgradable(buildingDescRef)
 
     if ( buildingDescRef.level > 1 ) then
@@ -140,7 +150,7 @@ function upgrade_full_map_picker_tool:OnActivateSelectorRequest()
         end
 
         local baseBuildingDesc = BuildingService:FindBaseBuilding( blueprintName )
-        if (baseBuildingDesc ~= nil ) then
+        if ( baseBuildingDesc ~= nil ) then
             buildingDesc = baseBuildingDesc
         end
 
@@ -180,6 +190,94 @@ function upgrade_full_map_picker_tool:OnActivateSelectorRequest()
         end
 
         ::continue::
+    end
+end
+
+function upgrade_tool:HighlightBuildingsToUpgrade()
+
+    -- Buildings within a radius radiusShowBuildingsToUpgrade from player to highlight
+    local buildings = self:FindUpgradableBuildings()
+
+    self.previousMarkedBuildings = self.previousMarkedBuildings or {}
+
+    -- Remove highlighting from previous buildings
+    for entity in Iter( self.previousMarkedBuildings ) do
+
+        -- If the building is not included in the new list
+        if ( IndexOf( buildings, entity ) ~= nil ) then
+            goto continue
+        end
+
+        if ( IndexOf( self.selectedEntities, entity ) ~= nil ) then
+            goto continue
+        end
+
+        self:RemovedFromSelection( entity )
+
+        ::continue::
+    end
+
+    for entity in Iter( buildings ) do
+
+        -- Highlight building if it can be upgraded
+        local skinned = EntityService:IsSkinned(entity)
+        if ( skinned ) then
+            EntityService:SetMaterial( entity, "selector/hologram_active_skinned", "selected" )
+        else
+            EntityService:SetMaterial( entity, "selector/hologram_active", "selected" )
+        end
+    end
+
+    self.previousMarkedBuildings = buildings
+end
+
+function upgrade_tool:FindUpgradableBuildings()
+
+    local player = PlayerService:GetPlayerControlledEnt(self.playerId)
+
+    -- Buildings within a radius radiusShowBuildingsToUpgrade from player to highlight
+    local buildings = FindService:FindEntitiesByTypeInRadius( player, "building", self.radiusShowBuildingsToUpgrade )
+
+    local result = {}
+
+    for entity in Iter( buildings ) do
+
+        -- Skip buildins from result
+        if ( IndexOf( result, entity ) ~= nil ) then
+            goto continue
+        end
+
+        -- Skip buildins from self.selectedEntities
+        if ( IndexOf( self.selectedEntities, entity ) ~= nil ) then
+            goto continue
+        end
+
+        -- Highlight building if it can be upgraded
+        if ( not BuildingService:CanUpgrade( entity, self.playerId ) ) then
+            goto continue
+        end
+
+        Insert( result, entity )
+
+        ::continue::
+    end
+
+    return result
+end
+
+function upgrade_tool:OnRelease()
+
+    -- Remove highlighting from buildings
+    if ( self.previousMarkedBuildings ~= nil ) then
+        for ent in Iter( self.previousMarkedBuildings ) do
+
+            self:RemovedFromSelection( ent )
+        end
+    end
+    self.previousMarkedBuildings = {}
+
+    if ( upgrade_full_map_base.OnRelease ) then
+        upgrade_full_map_base.OnRelease(self)
     end
 end
 
