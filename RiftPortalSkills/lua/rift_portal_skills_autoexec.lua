677d49df9671b08b706d89b969a48ad7efe90940
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-06-24 20:56:23 +0300

    RiftPortalSkills. Fixing the duplicate skill bug.

--- RiftPortalSkills/lua/rift_portal_skills_autoexec.lua
+++ RiftPortalSkills/lua/rift_portal_skills_autoexec.lua
@@ -7,7 +7,35 @@ RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     local player = PlayerService:GetPlayerControlledEnt( playerId )
 
-    if ( player == nil ) then
+    if ( player == nil or player == INVALID_ID ) then
+        return
+    end
+
+    local skillList = {
+
+        "items/skills/rift_portal_temporary",
+        "items/skills/rift_portal_personal",
+        "items/skills/rift_jump_to_hq",
+        "items/skills/rift_jump_to_nearest_portal"
+    }
+
+    for skillName in Iter( skillList ) do
+
+        local itemCount = ItemService:GetItemCount( player, skillName )
+
+        if ( itemCount == 0 ) then
+            PlayerService:AddItemToInventory( playerId, skillName )
+        end
+    end
+end)
+
+RegisterGlobalEventHandler("PlayerControlledEntityChangeEvent", function(evt)
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetPlayerControlledEnt( playerId )
+
+    if ( player == nil or player == INVALID_ID ) then
         return
     end
 
