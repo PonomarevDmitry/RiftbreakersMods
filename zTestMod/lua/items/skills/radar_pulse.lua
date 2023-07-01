6d92c5035a5db67e05605b06a1138a4db6644142
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-07-01 11:27:04 +0300

    zTestMod. Correct radar pulse to use blueprint database.

---
+++ zTestMod/lua/items/skills/radar_pulse.lua
@@ -0,0 +1,35 @@
+local item = require("lua/items/item.lua")
+
+require("lua/utils/reflection.lua")
+
+class 'radar_pulse' ( item )
+
+function radar_pulse:__init()
+    item.__init(self,self)
+end
+
+function radar_pulse:OnInit()
+end
+
+function radar_pulse:OnEquipped()
+end
+
+function radar_pulse:OnActivate()
+    local radarPulseEffect = EntityService:SpawnEntity( "items/consumables/radar_pulse", self.entity, "")
+
+    local radarRevealer = EntityService:GetComponent(radarPulseEffect, "FogOfWarRevealerComponent" )
+    if ( radarRevealer == nil ) then
+        Assert( false, "ERROR: No fog of war revealer component:" )
+    end
+    
+    local helper = reflection_helper( radarRevealer )
+
+    local database = EntityService:GetBlueprintDatabase( self.entity ) or self.data
+
+    helper.radius = database:GetFloatOrDefault( "radius", 100 ) 
+    local lifeTime = database:GetFloatOrDefault("life_time", 10 )
+
+    EntityService:CreateOrSetLifetime( radarPulseEffect, lifeTime, "normal" )
+end
+
+return radar_pulse
