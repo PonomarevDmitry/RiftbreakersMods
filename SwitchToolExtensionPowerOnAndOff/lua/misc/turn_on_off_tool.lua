3c1b3c43ef4ecdb2ad861614e7606abe8b2c54e5
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-07-01 15:16:07 +0300

    SwitchToolExtensionPowerOnAndOff. Change icons and lua-script.

--- SwitchToolExtensionPowerOnAndOff/lua/misc/turn_on_off_tool.lua
+++ SwitchToolExtensionPowerOnAndOff/lua/misc/turn_on_off_tool.lua
@@ -9,15 +9,10 @@ end
 
 function turn_on_ff_tool:OnInit()
 
-    local setPower = ( self.data:GetIntOrDefault("set_power", 0) == 1 )
+    self.setPower = ( self.data:GetIntOrDefault("set_power", 0) == 1 )
 
-    self.setPower = setPower
-
-    if ( setPower ) then
-        self.childEntity = EntityService:SpawnAndAttachEntity("misc/marker_selector_turn_on", self.entity)
-    else
-        self.childEntity = EntityService:SpawnAndAttachEntity("misc/marker_selector_turn_off", self.entity)
-    end
+    local marker_name = self.data:GetString("marker_name")
+    self.childEntity = EntityService:SpawnAndAttachEntity( marker_name, self.entity )
 
     self.popupShown = false
 end
@@ -27,27 +22,27 @@ function turn_on_ff_tool:AddedToSelection( entity )
     local skinned = EntityService:IsSkinned(entity)
 
     if ( skinned ) then
-        EntityService:SetMaterial( entity, "selector/hologram_current_skinned", "selected")
+        EntityService:SetMaterial( entity, "selector/hologram_current_skinned", "selected" )
     else
-        EntityService:SetMaterial( entity, "selector/hologram_current", "selected")
+        EntityService:SetMaterial( entity, "selector/hologram_current", "selected" )
     end
 end
 
 function turn_on_ff_tool:SpawnCornerBlueprint()
     if ( self.corners == nil ) then
-        self.corners = EntityService:SpawnAndAttachEntity("misc/marker_selector_corner_tool_violet", self.entity )
+        self.corners = EntityService:SpawnAndAttachEntity( "misc/marker_selector_corner_tool_violet", self.entity )
     end
 end
 
 function turn_on_ff_tool:RemovedFromSelection( entity )
-    EntityService:RemoveMaterial(entity, "selected" )
+    EntityService:RemoveMaterial( entity, "selected" )
 end
 
 function turn_on_ff_tool:FilterSelectedEntities( selectedEntities )
 
     local entities = {}
 
-    for ent in Iter(selectedEntities ) do
+    for ent in Iter( selectedEntities ) do
 
         if ( EntityService:GetComponent(ent, "ResourceConverterComponent") ~= nil ) then
 
@@ -116,8 +111,8 @@ end
 
 function turn_on_ff_tool:OnActivateEntity( entity )
 
-    if( BuildingService:IsPlayerWorking( entity ) ~= self.setPower )  then
-        QueueEvent("ChangeBuildingStatusRequest", entity, self.setPower )
+    if( BuildingService:IsPlayerWorking( entity ) ~= self.setPower ) then
+        QueueEvent( "ChangeBuildingStatusRequest", entity, self.setPower )
     end
 end
 
