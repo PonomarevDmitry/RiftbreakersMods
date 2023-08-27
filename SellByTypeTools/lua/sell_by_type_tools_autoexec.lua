7f8bb935f9c6cae04c87efae21946c7d1b13c56a
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-08-27 23:46:54 +0300

    SellByTypeTools. Adding seller by buildingdesc.type.

--- SellByTypeTools/lua/sell_by_type_tools_autoexec.lua
+++ SellByTypeTools/lua/sell_by_type_tools_autoexec.lua
@@ -4,7 +4,9 @@ RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller")
     BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller_group")
+    BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller_group_type")
 
     BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller_ruin")
     BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller_ruin_group")
+    BuildingService:UnlockBuilding("buildings/tools/sell_by_type_seller_ruin_group_type")
 end)
