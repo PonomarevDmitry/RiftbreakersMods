004e982b82b1ddf17912cf5f913d396fe710ecde
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-09-02 11:06:49 +0300

    WrecksEraser.

---
+++ WrecksEraser/lua/misc/eraser_wrecks_tool.lua
@@ -0,0 +1,110 @@
+local tool = require("lua/misc/tool.lua")
+require("lua/utils/table_utils.lua")
+
+class 'eraser_wrecks_tool' ( tool )
+
+function eraser_wrecks_tool:__init()
+    tool.__init(self,self)
+end
+
+function eraser_wrecks_tool:OnInit()
+    self.childEntity = EntityService:SpawnAndAttachEntity("misc/marker_selector_eraser_wrecks_tool", self.entity)
+end
+
+function eraser_wrecks_tool:SpawnCornerBlueprint()
+    if ( self.corners == nil ) then
+        self.corners = EntityService:SpawnAndAttachEntity("misc/marker_selector_corner_tool_gold", self.entity )
+    end
+end
+
+function eraser_wrecks_tool:GetScaleFromDatabase()
+    return { x=1, y=1, z=1 }
+end
+
+function eraser_wrecks_tool:FindEntitiesToSelect( selectorComponent )
+
+    local predicate = {
+
+        signature="TypeComponent",
+
+        filter = function(entity)
+
+            if ( EntityService:CompareType( entity, "wreck" ) ) then
+                return true
+            end
+
+            if ( EntityService:CompareType( entity, "wreck_small" ) ) then
+                return true
+            end
+
+            if ( EntityService:CompareType( entity, "wreck_medium" ) ) then
+                return true
+            end
+
+            if ( EntityService:CompareType( entity, "wreck_large" ) ) then
+                return true
+            end
+
+            return false
+        end
+    };
+
+    local position = selectorComponent.position
+
+    local boundsSize = { x=1.0, y=1.0, z=1.0 }
+
+    local minVector = VectorSub(position, VectorMulByNumber(boundsSize, self.currentScale))
+    local maxVector = VectorAdd(position, VectorMulByNumber(boundsSize, self.currentScale))
+
+    local tempCollection = FindService:FindEntitiesByPredicateInBox( minVector, maxVector, predicate )
+
+    local possibleSelectedEnts = {}
+
+    for entity in Iter( tempCollection ) do
+
+        if ( entity == nil ) then
+            goto continue
+        end
+
+        if ( IndexOf( possibleSelectedEnts, entity ) ~= nil ) then
+            goto continue
+        end
+
+        Insert( possibleSelectedEnts, entity )
+
+        ::continue::
+    end
+
+    local selectorPosition = selectorComponent.position
+
+    local sorter = function( t, lhs, rhs )
+        local p1 = EntityService:GetPosition( lhs )
+        local p2 = EntityService:GetPosition( rhs )
+        local d1 = Distance( selectorPosition, p1 )
+        local d2 = Distance( selectorPosition, p2 )
+        return d1 < d2
+    end
+
+    table.sort(possibleSelectedEnts, function(a,b)
+        return sorter(possibleSelectedEnts, a, b)
+    end)
+
+    return possibleSelectedEnts
+end
+
+function eraser_wrecks_tool:AddedToSelection( entity )
+end
+
+function eraser_wrecks_tool:RemovedFromSelection( entity )
+end
+
+function eraser_wrecks_tool:OnRotate()
+end
+
+function eraser_wrecks_tool:OnActivateEntity( entity )
+
+    local dissolveTime = RandFloat( 1.0, 2.0 )
+    EntityService:DissolveEntity( entity, dissolveTime )
+end
+
+return eraser_wrecks_tool
