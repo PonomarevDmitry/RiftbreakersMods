cb57701b9b4e0e284dd0a152d6f6dcafab18f39f
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-09-09 14:58:11 +0300

    UpgradeAllMapTools. Adding upgrade all map by building group.

--- UpgradeAllMapTools/lua/misc/upgrade_all_map_upgrader_tool.lua
+++ UpgradeAllMapTools/lua/misc/upgrade_all_map_upgrader_tool.lua
@@ -14,14 +14,49 @@ end
 
 function upgrade_all_map_upgrader_tool:OnInit()
 
-    local marker_name = self.data:GetString("marker_name")
-    self.childEntity = EntityService:SpawnAndAttachEntity(marker_name, self.entity)
-
     self.scaleMap = {
         1,
     }
 
     self:InitLowUpgradeList()
+
+    self:SetTypeSetting()
+
+    self.isGroup = false
+    self.currentChildIsGroup = nil
+
+    self:UpdateMarker()
+end
+
+function upgrade_all_map_upgrader_tool:UpdateMarker()
+
+    local messageText = ""
+    local groupString = ""
+
+    local markerBlueprint = self.data:GetString("marker_name")
+
+    if ( self.isGroup ) then
+        messageText = "gui/hud/upgrade_all_map/building_group"
+        groupString = "_by_group"
+
+        markerBlueprint = self.data:GetString("marker_group")
+    end
+
+    if ( self.childEntity == nil or self.currentChildIsGroup ~= self.isGroup ) then
+
+        -- Destroy old marker
+        if (self.childEntity ~= nil) then
+
+            EntityService:RemoveEntity(self.childEntity)
+            self.childEntity = nil
+        end
+
+        
+        -- Create new marker
+        self.childEntity = EntityService:SpawnAndAttachEntity(markerBlueprint, self.entity)
+
+        self.currentChildIsGroup = self.isGroup
+    end
 
     local markerDB = EntityService:GetDatabase( self.childEntity )
 
@@ -34,7 +69,7 @@ function upgrade_all_map_upgrader_tool:OnInit()
         if ( menuIcon ~= "" ) then
 
             markerDB:SetString("building_icon", menuIcon)
-            markerDB:SetString("message_text", "")
+            markerDB:SetString("message_text", messageText)
         else
 
             markerDB:SetString("building_icon", "gui/menu/research/icons/missing_icon_big")
@@ -45,6 +80,32 @@ function upgrade_all_map_upgrader_tool:OnInit()
         markerDB:SetString("building_icon", "gui/menu/research/icons/missing_icon_big")
         markerDB:SetString("message_text", "gui/hud/upgrade_all_map/building_not_selected")
     end
+end
+
+function upgrade_all_map_upgrader_tool:SetTypeSetting()
+
+    self.selectedType = ""
+
+    if ( self.selectedBuildingBlueprint == "" ) then
+        return
+    end
+
+    if ( not ResourceManager:ResourceExists( "EntityBlueprint", self.selectedBuildingBlueprint ) ) then
+        return
+    end
+
+    local buildingDesc = BuildingService:GetBuildingDesc( self.selectedBuildingBlueprint )
+    if ( buildingDesc == nil ) then
+        return
+    end
+
+    local buildingDescRef = reflection_helper( buildingDesc )
+
+    if ( buildingDescRef.type == nil or buildingDescRef.type == "" ) then
+        return
+    end
+
+    self.selectedType = buildingDescRef.type
 end
 
 function upgrade_all_map_upgrader_tool:GetScaleFromDatabase()
@@ -138,31 +199,7 @@ function upgrade_all_map_upgrader_tool:FindEntitiesToSelect( selectorComponent )
             goto continue
         end
 
-        if ( not EntityService:HasComponent( entity, "BuildingComponent" ) ) then
-            goto continue
-        end
-
-        if ( not EntityService:HasComponent( entity, "SelectableComponent" ) ) then
-            goto continue
-        end
-
-        local blueprintName = EntityService:GetBlueprintName( entity )
-
-        if ( not self:IsBlueprintInList(blueprintName) ) then
-            goto continue
-        end
-
-        local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
-        if ( buildingDesc == nil ) then
-            goto continue
-        end
-
-        local buildingDescRef = reflection_helper( buildingDesc )
-        if ( buildingDescRef == nil ) then
-            goto continue
-        end
-
-        if ( not BuildingService:CanUpgrade( entity, self.playerId ) ) then
+        if ( not self:IsEntityApproved(entity) ) then
             goto continue
         end
 
@@ -186,7 +223,110 @@ function upgrade_all_map_upgrader_tool:FindEntitiesToSelect( selectorComponent )
     return result
 end
 
-function upgrade_all_map_upgrader_tool:OnRotate()
+function upgrade_all_map_upgrader_tool:IsEntityApproved( entity )
+
+    local buildingComponent = EntityService:GetComponent( entity, "BuildingComponent" )
+    if ( buildingComponent == nil ) then
+        return false
+    end
+
+    if ( not EntityService:HasComponent( entity, "SelectableComponent" ) ) then
+        return false
+    end
+
+    local blueprintName = EntityService:GetBlueprintName(entity)
+
+    local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
+    if ( buildingDesc == nil ) then
+        return false
+    end
+
+    if ( not BuildingService:CanUpgrade( entity, self.playerId ) ) then
+        return false
+    end
+
+    if ( self.isGroup ) then
+
+        if ( self:IsBlueprintInLowNameList(blueprintName) ) then
+            return true
+        end
+
+        if ( self.selectedType == "tower" or self.selectedType == "trap" or self.selectedType == "gate" ) then
+
+            local buildingDescRef = reflection_helper( buildingDesc )
+
+            if ( buildingDescRef.type == self.selectedType ) then
+                return true
+            end
+        end
+    else
+
+        if ( self:IsBlueprintInList(blueprintName) ) then
+            return true
+        end
+    end
+
+    return false
+end
+
+function upgrade_all_map_upgrader_tool:OnRotateSelectorRequest(evt)
+
+    local degree = evt:GetDegree()
+
+    local change = 1
+    if ( degree > 0 ) then
+        change = -1
+    end
+
+    local currentGroupValue = self:CheckGroupValueExists(self.isGroup)
+
+    local groupValuesArray = self:GetGroupValuesArray()
+
+    local index = IndexOf( groupValuesArray, currentGroupValue )
+    if ( index == nil ) then
+        index = 1
+    end
+
+    local maxIndex = #groupValuesArray
+
+    local newIndex = index + change
+    if ( newIndex > maxIndex ) then
+        newIndex = maxIndex
+    elseif( newIndex <= 0 ) then
+        newIndex = 1
+    end
+
+    local newValue = groupValuesArray[newIndex]
+
+    self.isGroup = newValue
+
+    self:UpdateMarker()
+end
+
+function upgrade_all_map_upgrader_tool:CheckGroupValueExists( groupValue )
+
+    local groupValuesArray = self:GetGroupValuesArray()
+
+    groupValue = groupValue or groupValuesArray[1]
+
+    local index = IndexOf(groupValuesArray, groupValue )
+
+    if ( index == nil ) then
+
+        return groupValuesArray[1]
+    end
+
+    return groupValue
+end
+
+function upgrade_all_map_upgrader_tool:GetGroupValuesArray()
+
+    if ( self.groupValuesArray == nil ) then
+
+        self.groupValuesArray = { false, true }
+    end
+
+    return self.groupValuesArray
 end
 
 function upgrade_all_map_upgrader_tool:OnActivateSelectorRequest()
