7b5818e6024e8c573dfb01072809dd8a4c803a3a
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-10-16 00:46:28 +0300

    MinesEraser. Save and load tool size.

--- MinesEraser/lua/misc/eraser_mines_tool.lua
+++ MinesEraser/lua/misc/eraser_mines_tool.lua
@@ -17,10 +17,6 @@ function eraser_mines_tool:SpawnCornerBlueprint()
     end
 end
 
-function eraser_mines_tool:GetScaleFromDatabase()
-    return { x=1, y=1, z=1 }
-end
-
 function eraser_mines_tool:FindEntitiesToSelect( selectorComponent )
 
     local predicate = {
