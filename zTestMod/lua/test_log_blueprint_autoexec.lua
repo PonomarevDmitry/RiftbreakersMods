2a0a8bd1b4e197ca10298cebe27efe41ee18e2dd
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-11-11 11:31:23 +0300

    zTestMod. Comments.

--- zTestMod/lua/test_log_blueprint_autoexec.lua
+++ zTestMod/lua/test_log_blueprint_autoexec.lua
@@ -1,6 +1,54 @@
 require("lua/utils/reflection.lua")
 require("lua/utils/table_utils.lua")
 
+--test_log_blueprint items/loot/saplings/crystal_metallic_red_item
+
+--test_log_blueprint items/loot/saplings/crystal_metallic_red_item InventoryItemComponent
+--test_log_blueprint items/loot/saplings/crystal_metallic_red_item GatherResourceComponent
+--test_log_blueprint items/loot/saplings/crystal_metallic_red_item DatabaseComponent
+
+--test_log_blueprint buildings/energy/carbonium_powerplant
+
+--test_log_blueprint buildings/energy/carbonium_powerplant AnimationGraphDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant BuildingComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant BuildingDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant BuildingStatusComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant DatabaseComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant DestroyDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant DestructionLevelComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant EffectDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant FogOfWarRevealerComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant GridCullerComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant HealthBarComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant HealthComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant HealthDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant LootComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant LuaComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant MeshDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant MinimapItemComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant NodeCullerComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant PhysicsDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant ResourceConverterComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant ResourceConverterDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant ResourceStorageComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant SkeletonDesc
+--test_log_blueprint buildings/energy/carbonium_powerplant TypeComponent
+--test_log_blueprint buildings/energy/carbonium_powerplant UniformComponent
+
+
+--test_log_blueprint resources/resource_sludge -all
+
+--test_log_blueprint spawners/loot_container_spawner -all
+
+--test_log_blueprint spawners/loot_container_spawner_advanced -all
+
+--test_log_blueprint buildings/main/headquarters -all
+
+--test_log_blueprint buildings/decorations/floor_acid_1x1 -all
+
+
+
+
 ConsoleService:RegisterCommand( "test_log_blueprint", function( args )
 
     if not Assert( #args == 1 or #args == 2, "Command test_log_blueprint requires one or two arguments! [blueprintName] [componentName] " .. tostring(#args) ) then
