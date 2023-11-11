d5ca59c2c9bfaf6697c9f249d180e7744e2a69bc
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-11-11 13:37:37 +0300

    EnergyConnectorTrail. When building desert floor, get base terrain, ignore mech trails.

--- EnergyConnectorTrail/lua/items/skills/energy_connector_trail.lua
+++ EnergyConnectorTrail/lua/items/skills/energy_connector_trail.lua
@@ -242,9 +242,9 @@ function energy_connector_trail:BuildOnSpot( spot )
 
     if ( BuildingService:IsBuildingAvailable( self.playerId, antiQuickSandFloor ) and BuildingService:CanAffordBuilding( antiQuickSandFloor, self.playerId) ) then
 
-        local terrainType = self:GetTerrainType( spot.position )
+        local buildDesertFloor = self:ShouldBuildDesertFloor( spot.position )
 
-        if ( terrainType == "quicksand" ) then
+        if ( buildDesertFloor ) then
 
             local transformFloor = {}
             transformFloor.position = spot.position
@@ -263,13 +263,60 @@ function energy_connector_trail:BuildOnSpot( spot )
     end
 end
 
-function energy_connector_trail:GetTerrainType( position )
+function energy_connector_trail:ShouldBuildDesertFloor( position )
 
-    local tempEntity = EntityService:SpawnEntity( position )
-    local terrainType = EnvironmentService:GetTerrainTypeUnderEntity( tempEntity )
-    EntityService:RemoveEntity(tempEntity)
+    local terrainType = ""
 
-    return terrainType
+    local overrideTerrains = {}
+
+    local terrainCellEntityId = EnvironmentService:GetTerrainCell(position)
+
+    if ( terrainCellEntityId ~= nil and terrainCellEntityId ~= INVALID_ID ) then
+        
+        local terrainTypeLayerComponent = EntityService:GetComponent( terrainCellEntityId, "TerrainTypeLayerComponent" )
+
+        if ( terrainTypeLayerComponent ~= nil ) then
+
+            local terrainTypeLayerComponentRef = reflection_helper(terrainTypeLayerComponent)
+
+            if ( terrainTypeLayerComponentRef.terrain_type and terrainTypeLayerComponentRef.terrain_type.resource and terrainTypeLayerComponentRef.terrain_type.resource.name ) then
+
+                terrainType = terrainTypeLayerComponentRef.terrain_type.resource.name
+            end
+        end
+        
+        local overrideTerrainComponent = EntityService:GetComponent( terrainCellEntityId, "OverrideTerrainComponent" )
+
+        if ( overrideTerrainComponent ~= nil ) then
+
+            local overrideTerrainComponentRef = reflection_helper(overrideTerrainComponent)
+
+            if ( overrideTerrainComponentRef.terrain_overrides ) then
+
+                for i=1,overrideTerrainComponentRef.terrain_overrides.count do
+
+                    local terrainTypeHolder = overrideTerrainComponentRef.terrain_overrides[i]
+
+                    if ( terrainTypeHolder and terrainTypeHolder.resource and terrainTypeHolder.resource.name ) then
+
+                        if ( IndexOf( overrideTerrains, terrainTypeHolder.resource.name ) == nil ) then
+                            Insert( overrideTerrains, terrainTypeHolder.resource.name )
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    local isQuickSand = (terrainType == "quicksand")
+    local hasDesertFloor = (IndexOf( overrideTerrains, "desert_floor" ) ~= nil)
+
+    if ( isQuickSand and not hasDesertFloor ) then
+
+        return true
+    end
+
+    return false
 end
 
 function energy_connector_trail:GetNearestSpot( playerPosition )
