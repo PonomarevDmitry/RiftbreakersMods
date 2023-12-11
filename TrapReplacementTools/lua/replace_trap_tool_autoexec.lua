173480c6ac12350ac53c70032575973a9323b1fb
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-12-11 23:13:35 +0300

    TrapReplacementTools. Adding last selected traps list.

--- TrapReplacementTools/lua/replace_trap_tool_autoexec.lua
+++ TrapReplacementTools/lua/replace_trap_tool_autoexec.lua
@@ -1,3 +1,9 @@
+require("lua/utils/string_utils.lua")
+require("lua/utils/table_utils.lua")
+require("lua/utils/reflection.lua")
+
+local LastSelectedBlueprintsListUtils = require("lua/utils/last_selected_blueprints_utils.lua")
+
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/tools/replace_trap_all_picker")
@@ -8,4 +14,46 @@ RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/tools/replace_trap_replacer_from_1_to_2")
     BuildingService:UnlockBuilding("buildings/tools/replace_trap_replacer_from_2_to_1")
+end)
+
+RegisterGlobalEventHandler("ChangeSelectorRequest", function(evt)
+
+    local blueprintName = evt:GetBlueprint()
+    local selector = evt:GetEntity()
+
+    local playerReferenceComponent = reflection_helper( EntityService:GetComponent(selector, "PlayerReferenceComponent") )
+    local playerId = playerReferenceComponent.player_id
+
+    local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
+    if ( buildingDesc == nil ) then
+        return
+    end
+
+    local buildingDescRef = reflection_helper(buildingDesc)
+    if ( buildingDescRef.category == "tools" ) then
+        return
+    end
+
+    if ( buildingDescRef.type ~= "trap" ) then
+        return
+    end
+
+    local blueprint = ResourceManager:GetBlueprint( blueprintName )
+    if ( blueprint == nil ) then
+        return
+    end
+
+    local buildingComponent = blueprint:GetComponent("BuildingComponent")
+    if ( buildingComponent == nil ) then
+        return
+    end
+
+    local list = BuildingService:GetBuildCosts( blueprintName, playerId )
+    if ( #list == 0 ) then
+        return
+    end
+
+    local parameterName = "$replace_trap_all.last_selected_buildings"
+
+    LastSelectedBlueprintsListUtils:AddBlueprintToList(parameterName, selector, blueprintName)
 end)
