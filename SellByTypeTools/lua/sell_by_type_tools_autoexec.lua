515a006b961fbe1c6d303072653be7f58f0d2f82
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-12-12 22:15:57 +0300

    SellByTypeTools. Checking blueprint is not null.

--- SellByTypeTools/lua/sell_by_type_tools_autoexec.lua
+++ SellByTypeTools/lua/sell_by_type_tools_autoexec.lua
@@ -17,7 +17,11 @@ end)
 
 RegisterGlobalEventHandler("ChangeSelectorRequest", function(evt)
 
-    local blueprintName = evt:GetBlueprint()
+    local blueprintName = evt:GetBlueprint() or ""
+    if ( blueprintName == "" or blueprintName == nil ) then
+        return
+    end
+
     local selector = evt:GetEntity()
 
     local playerReferenceComponent = reflection_helper( EntityService:GetComponent(selector, "PlayerReferenceComponent") )
