f6c9fdea93b2c34fc4677c84bbc08005946f4084
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2023-12-15 21:31:44 +0300

    TrapReplacementTools. Renaming files.

--- TrapReplacementTools/lua/replace_trap_tool_autoexec.lua
+++ TrapReplacementTools/lua/replace_trap_tool_autoexec.lua
@@ -2,7 +2,7 @@ require("lua/utils/string_utils.lua")
 require("lua/utils/table_utils.lua")
 require("lua/utils/reflection.lua")
 
-local LastSelectedBlueprintsListUtils = require("lua/utils/last_selected_blueprints_utils.lua")
+local LastSelectedBlueprintsListUtils = require("lua/utils/replace_trap_tool_last_selected_blueprints_utils.lua")
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
