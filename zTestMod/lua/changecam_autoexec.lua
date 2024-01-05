434794e45d5982c4aeed74c427d91e9cf70a6862
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-01-05 23:05:27 +0300

    zTestMod. Comment changecam_autoexec script.

--- zTestMod/lua/changecam_autoexec.lua
+++ zTestMod/lua/changecam_autoexec.lua
@@ -2,38 +2,38 @@ g_pitch_angle = -1.0471976995468
 
 RegisterGlobalEventHandler("EnterBuildMenuEvent", function(arg)
 
-    return
+    return;
 
-    local entity = CameraService:GetActiveCamera()
-    if entity == INVALID_ID then
-        return
-    end
-
-    local cameraOwner = EntityService:GetParent(entity)
-    if cameraOwner == INVALID_ID then
-        return
-    end
-
-    local followCameraComponent = reflection_helper( EntityService:GetComponent(cameraOwner, "FollowCameraControllerComponent") )
-    followCameraComponent.pitch_angle.radian = 30
+    --local entity = CameraService:GetActiveCamera()
+    --if entity == INVALID_ID then
+    --    return
+    --end
+    --
+    --local cameraOwner = EntityService:GetParent(entity)
+    --if cameraOwner == INVALID_ID then
+    --    return
+    --end
+    --
+    --local followCameraComponent = reflection_helper( EntityService:GetComponent(cameraOwner, "FollowCameraControllerComponent") )
+    --followCameraComponent.pitch_angle.radian = 30
 
 end)
 
 RegisterGlobalEventHandler("EnterFighterModeEvent", function(arg)
 
-    return
+    return;
 
-    local entity = CameraService:GetActiveCamera()
-    if entity == INVALID_ID then
-        return
-    end
-
-    local cameraOwner = EntityService:GetParent(entity)
-    if cameraOwner == INVALID_ID then
-        return
-    end
-
-    local followCameraComponent = reflection_helper( EntityService:GetComponent(cameraOwner, "FollowCameraControllerComponent") )
-    followCameraComponent.pitch_angle.radian = g_pitch_angle
+    --local entity = CameraService:GetActiveCamera()
+    --if entity == INVALID_ID then
+    --    return
+    --end
+    --
+    --local cameraOwner = EntityService:GetParent(entity)
+    --if cameraOwner == INVALID_ID then
+    --    return
+    --end
+    --
+    --local followCameraComponent = reflection_helper( EntityService:GetComponent(cameraOwner, "FollowCameraControllerComponent") )
+    --followCameraComponent.pitch_angle.radian = g_pitch_angle
 
 end)
