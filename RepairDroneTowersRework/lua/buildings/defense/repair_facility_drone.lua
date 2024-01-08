2d30c854aab368bfc123d6009b1772cae95baedb
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-01-09 00:42:06 +0300

    RepairDroneTowersRework. Removing loging.

--- RepairDroneTowersRework/lua/buildings/defense/repair_facility_drone.lua
+++ RepairDroneTowersRework/lua/buildings/defense/repair_facility_drone.lua
@@ -391,13 +391,11 @@ function repair_facility_drone:GettingInfoFromRuin()
 
         local blueprintName = EntityService:GetBlueprintName(ruinEntity)
         if ( blueprintName ~= selfRuinsBlueprint ) then
-            LogService:Log("GettingInfoFromRuin blueprintName ~= selfRuinsBlueprint")
             goto continue
         end
 
         local ruinPosition = EntityService:GetPosition(ruinEntity)
         if ( ruinPosition.x ~= position.x or ruinPosition.y ~= position.y or ruinPosition.z ~= position.z ) then
-            LogService:Log("GettingInfoFromRuin ruinPosition.x ~= position.x")
             goto continue
         end
 
@@ -405,7 +403,6 @@ function repair_facility_drone:GettingInfoFromRuin()
 
         local ruinDatabaseBlueprint = ruinDatabase:GetStringOrDefault("blueprint", "")
         if ( ruinDatabaseBlueprint ~= selfBlueprintName ) then
-            LogService:Log("GettingInfoFromRuin ruinDatabaseBlueprint ~= selfBlueprintName")
             goto continue
         end
 
