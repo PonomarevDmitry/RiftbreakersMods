a7ce6e081c0233b321473c50f43c5af0523d105d
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-02-20 21:00:53 +0300

    CustomizableGrenadesPack. Moving skills from research tree to autoexec.

---
+++ CustomizableGrenadesPack/lua/grenades_pack_autoexec.lua
@@ -0,0 +1,53 @@
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetPlayerControlledEnt( playerId )
+
+    if ( player == nil or player == INVALID_ID ) then
+        return
+    end
+
+    local skillList = {
+
+        "items/skills/grenades_pack_1_item",
+        "items/skills/grenades_pack_2_item",
+        "items/skills/grenades_pack_3_item"
+    }
+
+    for skillName in Iter( skillList ) do
+
+        local itemCount = ItemService:GetItemCount( player, skillName )
+
+        if ( itemCount == 0 ) then
+            PlayerService:AddItemToInventory( playerId, skillName )
+        end
+    end
+end)
+
+RegisterGlobalEventHandler("PlayerControlledEntityChangeEvent", function(evt)
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetPlayerControlledEnt( playerId )
+
+    if ( player == nil or player == INVALID_ID ) then
+        return
+    end
+
+    local skillList = {
+
+        "items/skills/grenades_pack_1_item",
+        "items/skills/grenades_pack_2_item",
+        "items/skills/grenades_pack_3_item"
+    }
+
+    for skillName in Iter( skillList ) do
+
+        local itemCount = ItemService:GetItemCount( player, skillName )
+
+        if ( itemCount == 0 ) then
+            PlayerService:AddItemToInventory( playerId, skillName )
+        end
+    end
+end)
