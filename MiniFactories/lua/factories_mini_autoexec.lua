f8f22d194a2bd0f0b895a63d9ee7a61b2dbee6e0
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-06 23:35:00 +0300

    Removing mod MiniFactories.

--- MiniFactories/lua/factories_mini_autoexec.lua
+++
@@ -1,7 +0,0 @@
-RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
-
-    BuildingService:UnlockBuilding("buildings/resources/factory_mini")
-    BuildingService:UnlockBuilding("buildings/resources/factory_mini_lvl_2")
-    BuildingService:UnlockBuilding("buildings/resources/factory_mini_lvl_3")
-
-end)
