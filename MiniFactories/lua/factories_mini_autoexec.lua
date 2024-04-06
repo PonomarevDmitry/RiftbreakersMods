16d7047b4099dc7b2126e27d8c307f446475bf21
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-06 23:15:18 +0300

    MiniFactories. Changing factories.

--- MiniFactories/lua/factories_mini_autoexec.lua
+++ MiniFactories/lua/factories_mini_autoexec.lua
@@ -1,15 +1,7 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
-    BuildingService:UnlockBuilding("buildings/resources/carbonium_factory_mini")
-    BuildingService:UnlockBuilding("buildings/resources/carbonium_factory_mini_lvl_2")
-    BuildingService:UnlockBuilding("buildings/resources/carbonium_factory_mini_lvl_3")
-
-    BuildingService:UnlockBuilding("buildings/resources/steel_factory_mini")
-    BuildingService:UnlockBuilding("buildings/resources/steel_factory_mini_lvl_2")
-    BuildingService:UnlockBuilding("buildings/resources/steel_factory_mini_lvl_3")
-
-    BuildingService:UnlockBuilding("buildings/resources/rare_element_mine_mini")
-    BuildingService:UnlockBuilding("buildings/resources/rare_element_mine_mini_lvl_2")
-    BuildingService:UnlockBuilding("buildings/resources/rare_element_mine_mini_lvl_3")
+    BuildingService:UnlockBuilding("buildings/resources/factory_mini")
+    BuildingService:UnlockBuilding("buildings/resources/factory_mini_lvl_2")
+    BuildingService:UnlockBuilding("buildings/resources/factory_mini_lvl_3")
 
 end)
