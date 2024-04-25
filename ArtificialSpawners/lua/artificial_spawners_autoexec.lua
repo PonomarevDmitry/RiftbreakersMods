9cc81c41252ce107b0d7408a2783f5eb9a82f3e9
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-25 21:29:22 +0300

    ArtificialSpawners. Tools.

--- ArtificialSpawners/lua/artificial_spawners_autoexec.lua
+++ ArtificialSpawners/lua/artificial_spawners_autoexec.lua
@@ -1,4 +1,7 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/main/artificial_spawner")
+
+    BuildingService:UnlockBuilding("buildings/tools/artificial_spawner_slots_picker")
+    BuildingService:UnlockBuilding("buildings/tools/artificial_spawner_slots_replacer")
 end)
