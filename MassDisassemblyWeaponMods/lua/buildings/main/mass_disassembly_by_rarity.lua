485f427a3194567de50249ea40a87751cfb4930d
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-28 20:12:52 +0300

    MassDisassemblyWeaponMods. Disassembly mods by quality.

---
+++ MassDisassemblyWeaponMods/lua/buildings/main/mass_disassembly_by_rarity.lua
@@ -0,0 +1,202 @@
+local building = require("lua/buildings/building.lua")
+require("lua/utils/reflection.lua")
+require("lua/utils/string_utils.lua")
+
+class 'mass_disassembly_by_rarity' ( building )
+
+function mass_disassembly_by_rarity:__init()
+    building.__init(self,self)
+end
+
+function mass_disassembly_by_rarity:OnInit()
+
+    self:RegisterHandler( self.entity, "InteractWithEntityRequest", "OnInteractWithEntityRequest" )
+end
+
+function mass_disassembly_by_rarity:OnInteractWithEntityRequest( event )
+
+    local player = event:GetOwner()
+
+    local hashRarityBlueprint,hasItems = self:GetModsToDisassebly()
+    if ( hasItems == false ) then
+        return
+    end
+
+
+    local inventoryComponent = EntityService:GetComponent(player, "InventoryComponent")
+    if ( inventoryComponent == nil ) then
+        return
+    end
+
+    local inventoryComponentRef = reflection_helper( inventoryComponent )
+
+    while ( inventoryComponentRef.reference_entity ~= nil and inventoryComponentRef.reference_entity.id ~= INVALID_ID ) do
+
+        inventoryComponent = EntityService:GetComponent(inventoryComponentRef.reference_entity.id, "InventoryComponent")
+
+        if ( inventoryComponent == nil ) then
+            inventoryComponentRef = nil
+            break
+        end
+
+        inventoryComponentRef = reflection_helper( inventoryComponent )
+    end
+
+    if ( inventoryComponentRef == nil or inventoryComponentRef.inventory == nil or inventoryComponentRef.inventory.items == nil ) then
+        return
+    end
+
+
+
+    local inventoryItems = inventoryComponentRef.inventory.items
+
+    for itemNumber=1,inventoryItems.count do
+
+        local itemEntity = inventoryItems[itemNumber]
+
+        if ( itemEntity == nil or itemEntity.id == nil) then
+            goto continue
+        end
+
+        if ( not EntityService:IsAlive( itemEntity.id )) then
+            goto continue
+        end
+
+        local weaponModComponent = EntityService:GetComponent(itemEntity.id, "WeaponModComponent")
+        if ( weaponModComponent == nil ) then
+            goto continue
+        end
+
+        local weaponModComponentRef = reflection_helper( weaponModComponent )
+        if ( weaponModComponentRef.mod_data == nil ) then
+            goto continue
+        end
+
+        local rarity = weaponModComponentRef.mod_data.rarity
+
+        if ( hashRarityBlueprint[rarity] == nil ) then
+            goto continue
+        end
+
+        local hashByRarity = hashRarityBlueprint[rarity]
+
+        local itemBlueprintName = EntityService:GetBlueprintName(itemEntity.id)
+
+        hashByRarity[itemBlueprintName] = hashByRarity[itemBlueprintName] or {}
+
+        if ( IndexOf( hashByRarity[itemBlueprintName], itemEntity.id ) ~= nil ) then
+            goto continue
+        end
+
+        Insert(hashByRarity[itemBlueprintName], itemEntity.id)
+
+        ::continue::
+    end
+
+    local resourcesValues = {}
+
+    for rarity, blueprintList in pairs( hashRarityBlueprint ) do
+
+        for itemBlueprintName, itemList in pairs( blueprintList ) do
+
+            local blueprint = ResourceManager:GetBlueprint( itemBlueprintName )
+            if ( blueprint ~= nil ) then
+
+                local costDesc = blueprint:GetComponent("CostDesc")
+                if ( costDesc ~= nil ) then
+
+                    local costDescRef = reflection_helper(costDesc)
+                    if ( costDescRef ~= nil and costDescRef.account ~= nil ) then
+
+                        local account = costDescRef.account
+
+                        if ( account.count > 0 ) then
+
+                            for i = 1,account.count do
+
+                                local researchCost = account[i]
+
+                                if ( researchCost ~= nil and researchCost.resource ~= nil and researchCost.resource ~= "" and researchCost.count ~= nil and researchCost.count > 0 ) then
+
+                                    local sum = (researchCost.count / 2) * #itemList
+
+                                    resourcesValues[researchCost.resource] = resourcesValues[researchCost.resource] or 0
+
+                                    resourcesValues[researchCost.resource] = resourcesValues[researchCost.resource] + sum
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+
+            for itemEntity in Iter(itemList) do
+                EntityService:RemoveEntity( itemEntity )
+            end
+        end
+    end
+
+    for resource, sum in pairs( resourcesValues ) do
+
+        PlayerService:AddResourceAmount( resource, sum )
+    end
+
+    EffectService:SpawnEffect(self.entity, "effects/enemies_lesigian/lightning_explosion")
+end
+
+function mass_disassembly_by_rarity:GetModsToDisassebly()
+
+    local hasItems = false
+    local hashRarityBlueprint = {}
+
+    local equipmentComponent = EntityService:GetComponent(self.entity, "EquipmentComponent")
+    if ( equipmentComponent ) then
+
+        local equipment = reflection_helper( equipmentComponent ).equipment[1]
+
+        local slots = equipment.slots
+        for i=1,slots.count do
+
+            local slot = slots[i]
+
+            local modItem = ItemService:GetEquippedItem( self.entity, slot.name )
+            if ( modItem ~= nil and modItem ~= INVALID_ID ) then
+
+                local blueprintName = EntityService:GetBlueprintName(modItem)
+
+                LogService:Log("GetModsToDisassebly " .. slot.name .. " blueprintName " .. tostring(blueprintName) .. " modItem " .. tostring(modItem))
+
+                local weaponModComponent = EntityService:GetComponent(modItem, "WeaponModComponent")
+                if ( weaponModComponent ~= nil ) then
+
+                    local weaponModComponentRef = reflection_helper( weaponModComponent )
+
+                    LogService:Log("GetModsToDisassebly " .. tostring(weaponModComponentRef))
+
+                    if ( weaponModComponentRef.mod_data ~= nil ) then
+
+                        local rarity = weaponModComponentRef.mod_data.rarity
+
+                        hashRarityBlueprint[rarity] = hashRarityBlueprint[rarity] or {}
+
+                        local hashByRarity = hashRarityBlueprint[rarity]
+
+                        hashByRarity[blueprintName] = hashByRarity[blueprintName] or {}
+                    
+
+
+                        hasItems = true
+
+                        Insert(hashByRarity[blueprintName], modItem)
+
+                        QueueEvent( "EquipmentChangeRequest", self.entity, slot.name, 0, INVALID_ID )
+                    end
+                end
+            end
+        end
+    end
+
+    return hashRarityBlueprint, hasItems
+end
+
+return mass_disassembly_by_rarity
--- MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
+++ MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
@@ -1,5 +1,6 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
     BuildingService:UnlockBuilding("buildings/main/mass_disassembly")
+    BuildingService:UnlockBuilding("buildings/main/mass_disassembly_by_rarity")
     BuildingService:UnlockBuilding("buildings/main/mass_disassembly_enable_mods")
 end)
