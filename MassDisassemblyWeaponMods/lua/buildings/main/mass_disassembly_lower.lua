d5c640a1f5993791b936e3375e659000ebea9454
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-28 20:29:09 +0300

    MassDisassemblyWeaponMods. Disassembly mods and lower quality.

---
+++ MassDisassemblyWeaponMods/lua/buildings/main/mass_disassembly_lower.lua
@@ -0,0 +1,193 @@
+local building = require("lua/buildings/building.lua")
+require("lua/utils/reflection.lua")
+require("lua/utils/string_utils.lua")
+
+class 'mass_disassembly_lower' ( building )
+
+function mass_disassembly_lower:__init()
+    building.__init(self,self)
+end
+
+function mass_disassembly_lower:OnInit()
+
+    self:RegisterHandler( self.entity, "InteractWithEntityRequest", "OnInteractWithEntityRequest" )
+end
+
+function mass_disassembly_lower:OnInteractWithEntityRequest( event )
+
+    local player = event:GetOwner()
+
+    local hashItems,hasItems = self:GetModsToDisassebly()
+    if ( hasItems == false ) then
+        return
+    end
+
+
+    local inventoryComponent = EntityService:GetComponent(player, "InventoryComponent")
+    if ( inventoryComponent == nil ) then
+        return
+    end
+
+    local inventoryComponentRef = reflection_helper( inventoryComponent )
+
+    while ( inventoryComponentRef.reference_entity ~= nil and inventoryComponentRef.reference_entity.id ~= INVALID_ID ) do
+
+        inventoryComponent = EntityService:GetComponent(inventoryComponentRef.reference_entity.id, "InventoryComponent")
+
+        if ( inventoryComponent == nil ) then
+            inventoryComponentRef = nil
+            break
+        end
+
+        inventoryComponentRef = reflection_helper( inventoryComponent )
+    end
+
+    if ( inventoryComponentRef == nil or inventoryComponentRef.inventory == nil or inventoryComponentRef.inventory.items == nil ) then
+        return
+    end
+
+
+
+    local inventoryItems = inventoryComponentRef.inventory.items
+
+    for itemNumber=1,inventoryItems.count do
+
+        local itemEntity = inventoryItems[itemNumber]
+
+        if ( itemEntity == nil or itemEntity.id == nil) then
+            goto continue
+        end
+
+        if ( not EntityService:IsAlive( itemEntity.id )) then
+            goto continue
+        end
+
+        local itemBlueprintName = EntityService:GetBlueprintName(itemEntity.id)
+        if ( hashItems[itemBlueprintName] == nil ) then
+            goto continue
+        end
+
+        if ( IndexOf( hashItems[itemBlueprintName], itemEntity.id ) ~= nil ) then
+            goto continue
+        end
+
+        Insert(hashItems[itemBlueprintName], itemEntity.id)
+
+        ::continue::
+    end
+
+    local resourcesValues = {}
+
+    for itemBlueprintName, itemList in pairs( hashItems ) do
+
+        local blueprint = ResourceManager:GetBlueprint( itemBlueprintName )
+        if ( blueprint ~= nil ) then
+
+            local costDesc = blueprint:GetComponent("CostDesc")
+            if ( costDesc ~= nil ) then
+
+                local costDescRef = reflection_helper(costDesc)
+                if ( costDescRef ~= nil and costDescRef.account ~= nil ) then
+
+                    local account = costDescRef.account
+
+                    if ( account.count > 0 ) then
+
+                        for i = 1,account.count do
+
+                            local researchCost = account[i]
+
+                            if ( researchCost ~= nil and researchCost.resource ~= nil and researchCost.resource ~= "" and researchCost.count ~= nil and researchCost.count > 0 ) then
+
+                                local sum = (researchCost.count / 2) * #itemList
+
+                                resourcesValues[researchCost.resource] = resourcesValues[researchCost.resource] or 0
+
+                                resourcesValues[researchCost.resource] = resourcesValues[researchCost.resource] + sum
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        for itemEntity in Iter(itemList) do
+            EntityService:RemoveEntity( itemEntity )
+        end
+    end
+
+    for resource, sum in pairs( resourcesValues ) do
+
+        PlayerService:AddResourceAmount( resource, sum )
+    end
+
+    EffectService:SpawnEffect(self.entity, "effects/enemies_lesigian/lightning_explosion")
+end
+
+function mass_disassembly_lower:GetModsToDisassebly()
+
+    local hasItems = false
+    local hashItems = {}
+
+    local equipmentComponent = EntityService:GetComponent(self.entity, "EquipmentComponent")
+    if ( equipmentComponent ) then
+
+        local equipment = reflection_helper( equipmentComponent ).equipment[1]
+
+        local slots = equipment.slots
+        for i=1,slots.count do
+
+            local slot = slots[i]
+
+            local modItem = ItemService:GetEquippedItem( self.entity, slot.name )
+            if ( modItem ~= nil and modItem ~= INVALID_ID ) then
+
+                local itemBlueprintName = EntityService:GetBlueprintName(modItem)
+
+                hashItems[itemBlueprintName] = hashItems[itemBlueprintName] or {}
+
+                hasItems = true
+
+                Insert(hashItems[itemBlueprintName], modItem)
+
+                QueueEvent( "EquipmentChangeRequest", self.entity, slot.name, 0, INVALID_ID )
+
+
+
+                local additionalBlueprints = self:GetAdditionalBlueptints(itemBlueprintName)
+
+                for addBlueprintName in Iter(additionalBlueprints) do
+
+                    hashItems[addBlueprintName] = hashItems[addBlueprintName] or {}
+                end
+            end
+        end
+    end
+
+    return hashItems, hasItems
+end
+
+function mass_disassembly_lower:GetAdditionalBlueptints(itemBlueprint)
+
+    local result = {}
+
+    if string.find(itemBlueprint, "advanced") then
+
+        Insert( result, string.gsub( itemBlueprint, "advanced", "standard" ) )
+
+    elseif string.find(itemBlueprint, "superior") then
+    
+        Insert( result, string.gsub( itemBlueprint, "superior", "standard" ) )
+        Insert( result, string.gsub( itemBlueprint, "superior", "advanced" ) )
+
+    elseif string.find(itemBlueprint, "extreme") then
+
+        Insert( result, string.gsub( itemBlueprint, "extreme", "standard" ) )
+        Insert( result, string.gsub( itemBlueprint, "extreme", "advanced" ) )
+        Insert( result, string.gsub( itemBlueprint, "extreme", "superior" ) )
+    end
+
+    return result
+end
+
+return mass_disassembly_lower
