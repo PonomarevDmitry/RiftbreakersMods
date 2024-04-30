dd90911bea0b30dfd506033a42f3b46c0edddf2c
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-04-30 12:54:58 +0300

    ArtificialSpawners. Changing localization and research tree.

--- ArtificialSpawners/lua/artificial_spawners_autoexec.lua
+++ ArtificialSpawners/lua/artificial_spawners_autoexec.lua
@@ -1,7 +1,5 @@
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
 
-    BuildingService:UnlockBuilding("buildings/main/artificial_spawner")
-
     BuildingService:UnlockBuilding("buildings/tools/artificial_spawner_slots_picker")
     BuildingService:UnlockBuilding("buildings/tools/artificial_spawner_slots_replacer")
     BuildingService:UnlockBuilding("buildings/tools/artificial_spawner_slots_replacer_all_map")
