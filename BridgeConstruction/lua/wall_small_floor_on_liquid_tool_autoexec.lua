662ed1722fc2ecee33d58a496d035916ec76a1e7
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-06-18 23:58:19 +0300

    BridgeConstruction. Adding tool to build high floor on liquid pools.

---
+++ BridgeConstruction/lua/wall_small_floor_on_liquid_tool_autoexec.lua
@@ -0,0 +1,22 @@
+local wall_obstacles_tool_autoexec = function(evt)
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetPlayerControlledEnt( playerId )
+
+    if ( player == nil or player == INVALID_ID ) then
+        return
+    end
+
+    BuildingService:UnlockBuilding("buildings/defense/wall_small_floor_on_liquid_tool")
+end
+
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    wall_obstacles_tool_autoexec(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerControlledEntityChangeEvent", function(evt)
+
+    wall_obstacles_tool_autoexec(evt)
+end)
