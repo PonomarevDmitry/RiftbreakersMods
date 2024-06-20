0fabf38b173412425f6f86924dd5b56841f63e4b
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-06-21 01:45:23 +0300

    Update to WE3. Adding items and skills to player's Inventory.

--- CustomizableTurretsCluster/lua/turrets_cluster_autoexec.lua
+++ CustomizableTurretsCluster/lua/turrets_cluster_autoexec.lua
@@ -1,3 +1,5 @@
+require("lua/utils/table_utils.lua")
+
 local turrets_cluster_autoexec = function(evt)
 
     local playerId = evt:GetPlayerId()
@@ -14,6 +16,36 @@ local turrets_cluster_autoexec = function(evt)
         "items/skills/turrets_cluster_2_item",
         "items/skills/turrets_cluster_3_item"
     }
+
+    local inventorySystemDataComponent = EntityService:GetSingletonComponent("InventorySystemDataComponent")
+    if ( inventorySystemDataComponent ~= nil ) then
+
+        local hashItemUnlocked = {}
+
+        local inventorySystemDataComponentRef = reflection_helper( inventorySystemDataComponent )
+
+        local unlockedArray = inventorySystemDataComponentRef.unlocked
+
+        for i=1,unlockedArray.count do
+
+            local unlockedItem = unlockedArray[i]
+
+            if ( IndexOf( skillList, unlockedItem ) ~= nil ) then
+
+                hashItemUnlocked[unlockedItem] = true
+            end
+        end
+
+        for skillName in Iter( skillList ) do
+
+            if (hashItemUnlocked[skillName] == nil) then
+
+                local team = EntityService:GetTeam( player )
+
+                QueueEvent( "NewAwardEvent", INVALID_ID, skillName, true, team )
+            end
+        end
+    end
 
     local inventoryComponent = EntityService:GetComponent(player, "InventoryComponent")
     if ( inventoryComponent ~= nil ) then
