2d8dbe8b203949372e8bc78d4713b8f6ea8a61d3
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-04 21:07:01 +0300

    ElementalMovementSkillsIcons. Moving icons change to lua script.

--- ElementalMovementSkillsIcons/lua/change_movement_skills_icons_autoexec.lua
+++ ElementalMovementSkillsIcons/lua/change_movement_skills_icons_autoexec.lua
@@ -10,28 +10,59 @@ local InjectChangeBlueprintInventoryItemComponentIcons = function(blueprintsList
         for _,blueprintName in ipairs(list) do
 
             local blueprint = ResourceManager:GetBlueprint( blueprintName )
-
-            if ( blueprint ~= nil ) then
-
-                local inventoryItemComponent = blueprint:GetComponent("InventoryItemComponent")
-
-                if inventoryItemComponent ~= nil then
-    
-                    inventoryItemComponent:GetField("icon"):SetValue(icon)
-                    inventoryItemComponent:GetField("bigger_icon"):SetValue(biggerIcon)
-                else
-                
-                    LogService:Log("InjectChangeBlueprintInventoryItemComponentIcons Blueprint " .. blueprintName .. " InventoryItemComponent NOT EXISTS.")
-                end
-            else
+            if ( blueprint == nil ) then
 
                 LogService:Log("InjectChangeBlueprintInventoryItemComponentIcons Blueprint " .. blueprintName .. " NOT EXISTS.")
+                goto continue
             end
+
+            local inventoryItemComponent = blueprint:GetComponent("InventoryItemComponent")
+            if inventoryItemComponent == nil then
+                
+                LogService:Log("InjectChangeBlueprintInventoryItemComponentIcons Blueprint " .. blueprintName .. " InventoryItemComponent NOT EXISTS.")
+                goto continue
+            end
+    
+            inventoryItemComponent:GetField("icon"):SetValue(icon)
+            inventoryItemComponent:GetField("bigger_icon"):SetValue(biggerIcon)
+
+            ::continue::
         end
     end
 end
 
 local supported_item_blueprints = {
+
+    {
+        ["icon"] = "gui/hud/items_icons/skills/invisibility",
+        ["bigger_icon"] = "gui/menu/items_icons/skills/invisibility",
+        ["list"] = {
+
+            "items/skills/invisibility_short_item",
+        },
+    },
+
+    {
+        ["icon"] = "gui/hud/items_icons/skills/teleport",
+        ["bigger_icon"] = "gui/menu/items_icons/skills/teleport",
+        ["list"] = {
+
+            "items/skills/teleport_item",
+            "items/skills/teleport_short_item",
+        },
+    },
+
+    {
+        ["icon"] = "gui/hud/items_icons/skills/dodge_roll",
+        ["bigger_icon"] = "gui/menu/items_icons/skills/dodge_roll",
+        ["list"] = {
+
+            "items/skills/dodge_roll_item",
+        },
+    },
+
+
+
 
     {
         ["icon"] = "gui/hud/items_icons/skills/dash_acid",
