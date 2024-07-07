0f7a64eb915347ef009b5e5eeae48e7ed3d958be
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 00:15:26 +0300

    BridgeConstruction. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- BridgeConstruction/lua/wall_small_floor_on_liquid_tool_autoexec.lua
+++ BridgeConstruction/lua/wall_small_floor_on_liquid_tool_autoexec.lua
@@ -1,15 +1,17 @@
 local wall_obstacles_tool_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
     BuildingService:UnlockBuilding("buildings/defense/wall_small_floor_on_liquid_tool")
 end
+
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    wall_obstacles_tool_autoexec(evt)
+end)
 
 RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
