01c5814718ca3a00b5a5eb2a5932ade20ca0ee5a
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-07 22:33:18 +0300

    BuildExtraStableFloorOnlyOnQuickSand. Using PlayerInitializedEvent event instead of PlayerCreatedEvent event.

--- BuildExtraStableFloorOnlyOnQuickSand/lua/floor_desert_on_quicksand_tool_autoexec.lua
+++ BuildExtraStableFloorOnlyOnQuickSand/lua/floor_desert_on_quicksand_tool_autoexec.lua
@@ -39,7 +39,7 @@ local floor_desert_on_quicksand_tool_autoexec = function(evt)
     end
 end
 
-RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
     floor_desert_on_quicksand_tool_autoexec(evt)
 end)
