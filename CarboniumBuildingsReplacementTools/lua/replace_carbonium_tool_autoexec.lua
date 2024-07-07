057b10df3c92b0cefbbc12496e57d14b9be283d4
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 00:41:05 +0300

    CarboniumBuildingsReplacementTools. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- CarboniumBuildingsReplacementTools/lua/replace_carbonium_tool_autoexec.lua
+++ CarboniumBuildingsReplacementTools/lua/replace_carbonium_tool_autoexec.lua
@@ -9,6 +9,11 @@ local replace_carbonium_tool_autoexec = function(evt)
     BuildingService:UnlockBuilding("buildings/tools/replace_carbonium_to_2_factory")
 end
 
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    replace_carbonium_tool_autoexec(evt)
+end)
+
 RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
     replace_carbonium_tool_autoexec(evt)
