8c8ed2a485f2b7c288d1ed541006d123302c9fdc
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 00:23:25 +0300

    BuildingsTemplatesTools. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- BuildingsTemplatesTools/lua/buildings_templates_autoexec.lua
+++ BuildingsTemplatesTools/lua/buildings_templates_autoexec.lua
@@ -1,10 +1,7 @@
 local buildings_templates_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
@@ -103,6 +100,11 @@ local buildings_templates_autoexec = function(evt)
 
     BuildingService:UnlockBuilding("buildings/tools/buildings_upgrader")
 end
+
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    buildings_templates_autoexec(evt)
+end)
 
 RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
