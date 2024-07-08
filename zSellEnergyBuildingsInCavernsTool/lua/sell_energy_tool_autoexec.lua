99ec158f79c0b3f8aa7475b7ee06cbb885778a77
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 21:00:51 +0300

    zSellEnergyBuildingsInCavernsTool. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- zSellEnergyBuildingsInCavernsTool/lua/sell_energy_tool_autoexec.lua
+++ zSellEnergyBuildingsInCavernsTool/lua/sell_energy_tool_autoexec.lua
@@ -1,15 +1,17 @@
 local sell_energy_tool_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
     BuildingService:UnlockBuilding("buildings/tools/sell_3_energy")
 end
+
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    sell_energy_tool_autoexec(evt)
+end)
 
 RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
