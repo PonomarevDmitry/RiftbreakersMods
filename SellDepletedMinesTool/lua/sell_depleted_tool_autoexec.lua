43c55613b2b21797fc1b6c560dc5b86346ae489d
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 22:20:17 +0300

    SellDepletedMinesTool. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- SellDepletedMinesTool/lua/sell_depleted_tool_autoexec.lua
+++ SellDepletedMinesTool/lua/sell_depleted_tool_autoexec.lua
@@ -1,10 +1,7 @@
 local sell_depleted_tool_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
@@ -12,6 +9,11 @@ local sell_depleted_tool_autoexec = function(evt)
 end
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    sell_depleted_tool_autoexec(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
     sell_depleted_tool_autoexec(evt)
 end)
