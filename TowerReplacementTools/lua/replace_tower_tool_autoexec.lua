d999259bae6c8312a160c24d772019d5e911e578
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 22:34:41 +0300

    TowerReplacementTools. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- TowerReplacementTools/lua/replace_tower_tool_autoexec.lua
+++ TowerReplacementTools/lua/replace_tower_tool_autoexec.lua
@@ -6,11 +6,8 @@ local LastSelectedBlueprintsListUtils = require("lua/utils/replace_tower_tool_la
 
 local replace_tower_tool_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
@@ -25,6 +22,11 @@ local replace_tower_tool_autoexec = function(evt)
 end
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    replace_tower_tool_autoexec(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
     replace_tower_tool_autoexec(evt)
 end)
