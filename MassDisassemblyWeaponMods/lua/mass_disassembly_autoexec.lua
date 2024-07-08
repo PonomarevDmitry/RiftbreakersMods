241f0df39a3539ad13b2daf19c189dc2474899b0
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-08 21:13:54 +0300

    MassDisassemblyWeaponMods. Using PlayerInitializedEvent and PlayerCreatedEvent events.

--- MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
+++ MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
@@ -1,10 +1,7 @@
 local mass_disassembly_autoexec = function(evt)
 
-    local playerId = evt:GetPlayerId()
-
-    local player = PlayerService:GetPlayerControlledEnt( playerId )
-
-    if ( player == nil or player == INVALID_ID ) then
+    local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
+    if ( buildingSystemCampaignInfoComponent == nil ) then
         return
     end
 
@@ -15,6 +12,11 @@ local mass_disassembly_autoexec = function(evt)
 end
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    mass_disassembly_autoexec(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
 
     mass_disassembly_autoexec(evt)
 end)
