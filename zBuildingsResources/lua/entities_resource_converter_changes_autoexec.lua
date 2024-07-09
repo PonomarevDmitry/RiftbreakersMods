bb63f99f74f0516dc8a43d99521b0e3cd3507625
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-09 22:49:59 +0300

    zBuildingsResources. Changing ResourceConverterComponent.

--- zBuildingsResources/lua/entities_resource_converter_changes_autoexec.lua
+++ zBuildingsResources/lua/entities_resource_converter_changes_autoexec.lua
@@ -4,7 +4,7 @@ require("lua/utils/reflection.lua")
 -- test_log_blueprint player/player InventoryComponent
 -- test_log_blueprint player/player ResourceStorageComponent
 
-local InjectChangeBlueprintResourceConverterValues = function(blueprintName, direction, newResourceConverterValues)
+local InjectChangeBlueprintResourceConverterValues = function(blueprintName, direction, newResourceConverterValues, newResourceConverterComponentValues)
 
     local blueprint = ResourceManager:GetBlueprint( blueprintName )
     if ( blueprint == nil ) then
@@ -12,62 +12,140 @@ local InjectChangeBlueprintResourceConverterValues = function(blueprintName, dir
         return
     end
 
-    local resourceConverterDesc = blueprint:GetComponent("ResourceConverterDesc")
-    if ( resourceConverterDesc == nil ) then
-        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterDesc NOT EXISTS.")
-        return
-    end
+    if ( newResourceConverterValues ~= nil ) then
 
-    local inArray = resourceConverterDesc:GetField(direction):ToContainer()
-    if ( inArray == nil ) then
-        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterDesc:GetField('" .. direction .. "'):ToContainer() NOT EXISTS.")
-        return
-    end
+        local resourceConverterDesc = blueprint:GetComponent("ResourceConverterDesc")
+        if ( resourceConverterDesc == nil ) then
+            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterDesc NOT EXISTS.")
         
-    for i=0,inArray:GetItemCount()-1 do
+        else
+            local inArray = resourceConverterDesc:GetField(direction):ToContainer()
+            if ( inArray == nil ) then
+                LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterDesc:GetField('" .. direction .. "'):ToContainer() NOT EXISTS.")
 
-        local gameplayResourceObject = inArray:GetItem(i)
+            else
+
+                for i=0,inArray:GetItemCount()-1 do
+
+                    local gameplayResourceObject = inArray:GetItem(i)
             
-        if ( gameplayResourceObject == nil ) then
-            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' gameplayResourceObject == nil")
+                    if ( gameplayResourceObject == nil ) then
+                        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' gameplayResourceObject == nil")
 
-            goto continue
+                        goto continue
+                    end
+
+                    local gameplayResourceObjectRef = reflection_helper(gameplayResourceObject)
+                    if ( gameplayResourceObjectRef.group ~= 12 ) then
+                        goto continue
+                    end
+
+                    if ( gameplayResourceObjectRef.resource == nil ) then
+                        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint 'player/player' gameplayResourceObjectRef.resource == nil")
+
+                        goto continue
+                    end
+
+                    if ( gameplayResourceObjectRef.resource.resource == nil ) then
+                        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint 'player/player' gameplayResourceObjectRef.resource.resource == nil")
+
+                        goto continue
+                    end
+
+                    local resourceId = gameplayResourceObjectRef.resource.resource.id
+
+                    if ( resourceId == nil or resourceId == "" ) then
+                        LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceId == nil or resourceId == ''")
+
+                        goto continue
+                    end
+
+                    if ( newResourceConverterValues[resourceId] == nil ) then
+                        --LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' newResourceConverterValues[resourceId] == nil " .. tostring(resourceId))
+
+                        goto continue
+                    end
+
+                    gameplayResourceObject:GetField("value"):SetValue(newResourceConverterValues[resourceId])
+
+                    ::continue::
+                end
+            end
+        end
+    end
+
+
+
+    if ( newResourceConverterComponentValues ~= nil ) then
+
+        local convertedHash = {}
+
+        for key, value in pairs(newResourceConverterComponentValues) do
+
+            local keyHash = CalcHash(key)
+
+            convertedHash[keyHash] = value
+
+            --LogService:Log("InjectChangeBlueprintResourceConverterValues convertedHash Blueprint '" .. blueprintName .. "' key " .. key .. "  keyHash " .. keyHash .. " value = " .. tostring(value))
         end
 
-        local gameplayResourceObjectRef = reflection_helper(gameplayResourceObject)
-        if ( gameplayResourceObjectRef.group ~= 12 ) then
-            goto continue
+        local resourceConverterComponent = blueprint:GetComponent("ResourceConverterComponent")
+        if ( resourceConverterComponent == nil ) then
+
+            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterComponent NOT EXISTS.")
+        else
+
+            local resourcesArray = resourceConverterComponent:GetField("resources"):ToContainer()
+            if ( resourcesArray == nil ) then
+
+                LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceConverterComponent:GetField('resources'):ToContainer() NOT EXISTS.")
+            else
+
+                for i=0,resourcesArray:GetItemCount()-1 do
+
+                    local flatMap_GameplayResourceTypeResourceBasket = resourcesArray:GetItem(i)
+
+                    local flatMap_GameplayResourceTypeResourceBasketContainer = flatMap_GameplayResourceTypeResourceBasket:ToContainer()
+            
+                    for j=0,flatMap_GameplayResourceTypeResourceBasketContainer:GetItemCount()-1 do
+
+                        local pair_GameplayResourceType_ResourceBasket = flatMap_GameplayResourceTypeResourceBasketContainer:GetItem(j)
+
+                        if ( pair_GameplayResourceType_ResourceBasket ~= nil ) then
+
+                            local pair_GameplayResourceType_ResourceBasketValue = pair_GameplayResourceType_ResourceBasket:GetField("value")
+
+                            if ( pair_GameplayResourceType_ResourceBasketValue ~= nil ) then
+
+                                local resourceArray = pair_GameplayResourceType_ResourceBasketValue:GetField("resource"):ToContainer()
+
+                                if ( resourceArray == nil ) then
+
+                                    LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' pair_GameplayResourceType_ResourceBasketValue:GetField('resources'):ToContainer() NOT EXISTS.")
+                                else
+
+                                    for k=0,resourceArray:GetItemCount()-1 do
+                                        
+                                        local keyValueObject = resourceArray:GetItem(k)
+
+                                        local keyValueObjectRef = reflection_helper(keyValueObject)
+
+                                        if ( keyValueObjectRef.key ~= nil and keyValueObjectRef.key.hash ~= nil and convertedHash[keyValueObjectRef.key.hash] ~= nil ) then
+
+                                            keyValueObjectRef.value.value = convertedHash[keyValueObjectRef.key.hash]
+                                        end
+                                    end
+                                end
+                            else
+                                LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' pair_GameplayResourceType_ResourceBasketValue NOT EXISTS.")
+                            end
+                        else
+                            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' pair_GameplayResourceType_ResourceBasket NOT EXISTS.")
+                        end
+                    end
+                end
+            end
         end
-
-        if ( gameplayResourceObjectRef.resource == nil ) then
-            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint 'player/player' gameplayResourceObjectRef.resource == nil")
-
-            goto continue
-        end
-
-        if ( gameplayResourceObjectRef.resource.resource == nil ) then
-            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint 'player/player' gameplayResourceObjectRef.resource.resource == nil")
-
-            goto continue
-        end
-
-        local resourceId = gameplayResourceObjectRef.resource.resource.id
-
-        if ( resourceId == nil or resourceId == "" ) then
-            LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' resourceId == nil or resourceId == ''")
-
-            goto continue
-        end
-
-        if ( newResourceConverterValues[resourceId] == nil ) then
-            --LogService:Log("InjectChangeBlueprintResourceConverterValues Blueprint '" .. blueprintName .. "' newResourceConverterValues[resourceId] == nil " .. tostring(resourceId))
-
-            goto continue
-        end
-
-        gameplayResourceObject:GetField("value"):SetValue(newResourceConverterValues[resourceId])
-
-        ::continue::
     end
 end
 
@@ -75,7 +153,7 @@ local InjectChangeListBlueprintResourceConverterValues = function(blueprintResou
 
     for _, configObject in ipairs(blueprintResourceConverterValues) do
 
-        InjectChangeBlueprintResourceConverterValues(configObject.name, direction, configObject.resource_converter)
+        InjectChangeBlueprintResourceConverterValues(configObject.name, direction, configObject.resource_converter, configObject.resource_converter_comp)
     end
 end
 
@@ -87,6 +165,11 @@ local new_resource_converter_values = {
 
             ["biomass_animal"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_animal"] = 100000,
+        },
     },
     
     {
@@ -95,6 +178,11 @@ local new_resource_converter_values = {
 
             ["biomass_animal"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_animal"] = 100000,
+        },
     },
     
     {
@@ -102,6 +190,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["biomass_animal"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_animal"] = 100000,
         },
     },
 
@@ -113,6 +206,11 @@ local new_resource_converter_values = {
 
             ["carbon_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
+        },
     },
     
     {
@@ -121,6 +219,11 @@ local new_resource_converter_values = {
 
             ["carbon_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
+        },
     },
     
     {
@@ -128,6 +231,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["carbon_vein"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
         },
     },
 
@@ -139,6 +247,11 @@ local new_resource_converter_values = {
 
             ["uranium"] = "0.4",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium"] = 400000,
+        },
     },
     
     {
@@ -147,6 +260,11 @@ local new_resource_converter_values = {
 
             ["uranium"] = "0.4",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium"] = 400000,
+        },
     },
     
     {
@@ -154,6 +272,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["uranium"] = "0.4",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium"] = 400000,
         },
     },
 
@@ -165,6 +288,11 @@ local new_resource_converter_values = {
 
             ["biomass_plant"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
+        },
     },
     
     {
@@ -173,6 +301,11 @@ local new_resource_converter_values = {
 
             ["biomass_plant"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
+        },
     },
     
     {
@@ -180,6 +313,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["biomass_plant"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
         },
     },
 
@@ -191,6 +329,11 @@ local new_resource_converter_values = {
 
             ["biomass_plant"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
+        },
     },
     
     {
@@ -199,6 +342,11 @@ local new_resource_converter_values = {
 
             ["biomass_plant"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
+        },
     },
     
     {
@@ -206,6 +354,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["biomass_plant"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["biomass_plant"] = 100000,
         },
     },
 
@@ -217,6 +370,11 @@ local new_resource_converter_values = {
 
             ["carbon_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
+        },
     },
     
     {
@@ -225,6 +383,11 @@ local new_resource_converter_values = {
 
             ["carbon_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
+        },
     },
     
     {
@@ -232,6 +395,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["carbon_vein"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["carbon_vein"] = 100000,
         },
     },
 
@@ -244,6 +412,12 @@ local new_resource_converter_values = {
             ["titanium"] = "0.2",
             ["palladium"] = "0.2",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
+            ["palladium"] = 200000,
+        },
     },
     
     {
@@ -253,6 +427,12 @@ local new_resource_converter_values = {
             ["titanium"] = "0.2",
             ["palladium"] = "0.2",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
+            ["palladium"] = 200000,
+        },
     },
     
     {
@@ -261,6 +441,12 @@ local new_resource_converter_values = {
 
             ["titanium"] = "0.2",
             ["palladium"] = "0.2",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
+            ["palladium"] = 200000,
         },
     },
 
@@ -272,6 +458,11 @@ local new_resource_converter_values = {
 
             ["energy"] = "50",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["energy"] = 50000000,
+        },
     },
     
     {
@@ -280,6 +471,11 @@ local new_resource_converter_values = {
 
             ["energy"] = "75",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["energy"] = 75000000,
+        },
     },
     
     {
@@ -287,6 +483,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["energy"] = "100",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["energy"] = 100000000,
         },
     },
 
@@ -302,6 +503,15 @@ local new_resource_converter_values = {
             ["cobalt_vein"] = "0.1",
             ["voidinite_ore_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore_vein"] = 100000,
+            ["titanium_vein"] = 100000,
+            ["palladium_vein"] = 100000,
+            ["cobalt_vein"] = 100000,
+            ["voidinite_ore_vein"] = 100000,
+        },
     },
     
     {
@@ -313,6 +523,15 @@ local new_resource_converter_values = {
             ["palladium_vein"] = "0.1",
             ["cobalt_vein"] = "0.1",
             ["voidinite_ore_vein"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore_vein"] = 100000,
+            ["titanium_vein"] = 100000,
+            ["palladium_vein"] = 100000,
+            ["cobalt_vein"] = 100000,
+            ["voidinite_ore_vein"] = 100000,
         },
     },
     
@@ -326,6 +545,15 @@ local new_resource_converter_values = {
             ["cobalt_vein"] = "0.1",
             ["voidinite_ore_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore_vein"] = 100000,
+            ["titanium_vein"] = 100000,
+            ["palladium_vein"] = 100000,
+            ["cobalt_vein"] = 100000,
+            ["voidinite_ore_vein"] = 100000,
+        },
     },
 
 
@@ -336,6 +564,11 @@ local new_resource_converter_values = {
 
             ["iron_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["iron_vein"] = 100000,
+        },
     },
     
     {
@@ -344,6 +577,11 @@ local new_resource_converter_values = {
 
             ["iron_vein"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["iron_vein"] = 100000,
+        },
     },
     
     {
@@ -351,6 +589,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["iron_vein"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["iron_vein"] = 100000,
         },
     },
 
@@ -362,6 +605,11 @@ local new_resource_converter_values = {
 
             ["titanium"] = "0.2",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
+        },
     },
     
     {
@@ -370,6 +618,11 @@ local new_resource_converter_values = {
 
             ["titanium"] = "0.2",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
+        },
     },
     
     {
@@ -377,6 +630,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["titanium"] = "0.2",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["titanium"] = 200000,
         },
     },
 
@@ -388,6 +646,11 @@ local new_resource_converter_values = {
 
             ["uranium_ore"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore"] = 100000,
+        },
     },
     
     {
@@ -396,6 +659,11 @@ local new_resource_converter_values = {
 
             ["uranium_ore"] = "0.1",
         },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore"] = 100000,
+        },
     },
     
     {
@@ -403,6 +671,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["uranium_ore"] = "0.1",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["uranium_ore"] = 100000,
         },
     },
 }
@@ -416,6 +689,11 @@ local new_resource_converter_values = {
         ["resource_converter"] = {
 
             ["energy"] = "240",
+        },
+
+        ["resource_converter_comp"] = {
+
+            ["energy"] = 240000000,
         },
     },
 }
