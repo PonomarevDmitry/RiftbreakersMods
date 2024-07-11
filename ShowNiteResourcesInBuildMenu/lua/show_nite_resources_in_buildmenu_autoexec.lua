cdb92056e7c3d5c85a847a2fb251ad2950221592
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-11 22:51:12 +0300

    Moving changes in GameplayResourceDef to lua script.

---
+++ ShowNiteResourcesInBuildMenu/lua/show_nite_resources_in_buildmenu_autoexec.lua
@@ -0,0 +1,48 @@
+local InjectChangeGameplayResourceDefValues = function(resource, hudVisible, order)
+
+    local resourceDef = ResourceManager:GetResource("GameplayResourceDef", resource)
+    if ( resourceDef == nil ) then
+        LogService:Log("InjectChangeGameplayResourceDefValues GameplayResourceDef '" .. resource .. "' NOT EXISTS.")
+        return
+    end
+
+    resourceDef:GetField("order"):SetValue(order)
+    resourceDef:GetField("hud_visible"):SetValue(hudVisible)
+end
+
+local InjectChangeListGameplayResourceDefValues = function(blueprintResourceConverterValues)
+
+    for _, configObject in ipairs(blueprintResourceConverterValues) do
+
+        InjectChangeGameplayResourceDefValues(configObject.name, configObject.hud_visible, configObject.order)
+    end
+end
+
+local new_resource_values = {
+    
+    {
+        ["name"] = "hazenite",
+        ["hud_visible"] = "2", -- ResourceHudVisible.build_menu
+        ["order"] = "37",
+    },
+    
+    {
+        ["name"] = "tanzanite",
+        ["hud_visible"] = "2", -- ResourceHudVisible.build_menu
+        ["order"] = "38",
+    },
+    
+    {
+        ["name"] = "rhodonite",
+        ["hud_visible"] = "2", -- ResourceHudVisible.build_menu
+        ["order"] = "39",
+    },
+    
+    {
+        ["name"] = "ferdonite",
+        ["hud_visible"] = "2", -- ResourceHudVisible.build_menu
+        ["order"] = "40",
+    },
+}
+
+--InjectChangeListGameplayResourceDefValues(new_resource_values)
