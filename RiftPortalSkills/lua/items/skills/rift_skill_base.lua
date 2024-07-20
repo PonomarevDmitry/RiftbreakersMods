950ca9973de86a71945087be31f056dd6a246563
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-20 11:24:30 +0300

    RiftPortalSkills. Adding PlayerReferenceComponent to new spawned portal. Checking PlayerReferenceComponent.player_id before disposing other portals.

--- RiftPortalSkills/lua/items/skills/rift_skill_base.lua
+++ RiftPortalSkills/lua/items/skills/rift_skill_base.lua
@@ -82,17 +82,55 @@ end
 
 function rift_skill_base:SpawnPortal(blueprintName)
 
-    local entities = FindService:FindEntitiesByBlueprint( blueprintName )
-
-    for i=1,#entities do
-        QueueEvent( "DissolveEntityRequest", entities[i], 0.2, 0 )
+    local playerId = self:GetOwnerPlayerId()
+    if ( playerId == -1 ) then
+        return
     end
+
+    self:DissolveRirtPortalEntities(blueprintName, playerId)
 
     local team = EntityService:GetTeam( self.entity )
 
     local playerPosition = EntityService:GetPosition( self.owner )
 
     local newPortal = EntityService:SpawnEntity( blueprintName, playerPosition, team )
+
+    local playerReferenceComponentRef = reflection_helper(EntityService:CreateComponent(newPortal, "PlayerReferenceComponent"))
+    playerReferenceComponentRef.player_id = playerId
+    playerReferenceComponentRef.reference_type.internal_enum = 3
+end
+
+function rift_skill_base:DissolveRirtPortalEntities(blueprintName, playerId)
+
+    local entities = FindService:FindEntitiesByBlueprint( blueprintName )
+
+    for entity in Iter( entities ) do
+
+        local playerReferenceComponent = EntityService:GetComponent( entity, "PlayerReferenceComponent" )
+        if (playerReferenceComponent) then
+
+            local playerReferenceComponentRef = reflection_helper(playerReferenceComponent)
+
+            if (playerReferenceComponentRef and playerReferenceComponentRef.player_id == playerId) then
+
+                QueueEvent( "DissolveEntityRequest", entity, 0.2, 0 )
+            end
+        end
+    end
+end
+
+function rift_skill_base:GetOwnerPlayerId()
+
+    local playerId = -1
+
+    local playerReferenceComponent = EntityService:GetComponent( self.owner, "PlayerReferenceComponent" )
+    if (playerReferenceComponent) then
+
+        local playerReferenceComponentRef = reflection_helper(playerReferenceComponent)
+        playerId = playerReferenceComponentRef.player_id
+    end
+
+    return playerId
 end
 
 return rift_skill_base
