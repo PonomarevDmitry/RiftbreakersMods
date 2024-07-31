70fb2afc00215e20787cc2703e59e681f87759b1
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-07-31 22:22:57 +0300

    zTestMod. Adding old scanner tower research tree.

---
+++ zTestMod/lua/remove_scanner_tower_rt_autoexec.lua
@@ -0,0 +1,59 @@
+require("lua/utils/table_utils.lua")
+require("lua/utils/string_utils.lua")
+require("lua/utils/reflection.lua")
+
+local removeResearchTree = function(categoryName)
+
+    local researchComponent = EntityService:GetSingletonComponent( "ResearchSystemDataComponent" )
+    if ( researchComponent == nil ) then
+        return
+    end
+
+    local researchComponentRef = reflection_helper( researchComponent )
+
+    local categories = researchComponentRef.research
+
+    for i=1,categories.count do
+
+        local category = categories[i]
+
+        if ( category.category == categoryName ) then
+
+            LogService:Log("removeResearchTree category '" .. categoryName .. "' FOUNDED and REMOVED.")
+
+            local categoryContainer = researchComponent:GetField("research"):ToContainer()
+
+            categoryContainer:EraseItem( i - 1 )
+
+            return
+        end
+    end
+end
+
+local removeResearchTreeEvent = function(evt)
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetPlayerControlledEnt( playerId )
+
+    if ( player == nil or player == INVALID_ID ) then
+        return
+    end
+
+    removeResearchTree("gui/menu/research/category_mods")
+end
+
+RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
+
+    removeResearchTreeEvent(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerInitializedEvent", function(evt)
+
+    removeResearchTreeEvent(evt)
+end)
+
+RegisterGlobalEventHandler("PlayerControlledEntityChangeEvent", function(evt)
+
+    removeResearchTreeEvent(evt)
+end)
