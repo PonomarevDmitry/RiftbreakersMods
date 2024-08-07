460598b3be22d01cef116da61db049146382e78a
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-08-08 01:55:35 +0300

    LampTools.

---
+++ LampTools/lua/buildings/decorations/lamp_base_tool.lua
@@ -0,0 +1,364 @@
+require("lua/utils/reflection.lua")
+require("lua/utils/table_utils.lua")
+require("lua/utils/string_utils.lua")
+require("lua/utils/building_utils.lua")
+
+class 'lamp_base_tool' ( LuaEntityObject )
+
+function lamp_base_tool:__init()
+    LuaEntityObject.__init(self,self)
+end
+
+function lamp_base_tool:init()
+
+    self.stateMachine = self:CreateStateMachine()
+    self.stateMachine:AddState( "working", { execute="OnWorkExecute" } )
+    self.stateMachine:ChangeState("working")
+
+    if ( self.OnPreInit ) then
+        self:OnPreInit()
+    end
+
+    self:InitializeValues()
+
+    if ( self.OnInit ) then
+        self:OnInit()
+    end
+end
+
+function lamp_base_tool:InitializeValues()
+
+    self.selector = EntityService:GetParent( self.entity )
+
+    self:RegisterHandler( self.selector, "ActivateSelectorRequest",     "OnActivateSelectorRequest" )
+    self:RegisterHandler( self.selector, "DeactivateSelectorRequest",   "OnDeactivateSelectorRequest" )
+    self:RegisterHandler( self.selector, "RotateSelectorRequest",       "OnRotateSelectorRequest" )
+
+    local playerReferenceComponent = reflection_helper( EntityService:GetComponent( self.selector, "PlayerReferenceComponent" ) )
+    self.playerId = playerReferenceComponent.player_id
+
+    self.playerEntity = PlayerService:GetPlayerControlledEnt( self.playerId )
+
+    local boundsSize = EntityService:GetBoundsSize( self.selector )
+    self.boundsSize = VectorMulByNumber( boundsSize, 0.5 )
+
+    EntityService:ChangeMaterial( self.entity, "selector/hologram_blue" )
+    EntityService:SetVisible( self.entity , false )
+
+    self:CreateInfoChild()
+
+    self.announcements = {
+        ["ai"] = "voice_over/announcement/not_enough_ai_cores",
+
+        ["carbonium"] = "voice_over/announcement/not_enough_carbonium",
+        ["steel"] = "voice_over/announcement/not_enough_steel",
+
+        ["cobalt"] = "voice_over/announcement/not_enough_cobalt",
+        ["palladium"] = "voice_over/announcement/not_enough_palladium",
+        ["titanium"] = "voice_over/announcement/not_enough_titanium",
+        ["uranium"] = "voice_over/announcement/not_enough_uranium"
+    }
+
+    local selectorDB = EntityService:GetDatabase( self.selector )
+
+    self.lampBlueprintName = self:GetLampBlueprintName( selectorDB )
+
+    self:FillLampBuildingDesc( self.lampBlueprintName )
+
+    self:FindMinDistance()
+end
+
+function lamp_base_tool:CreateInfoChild()
+
+    if ( self.infoChild == nil ) then
+        self.infoChild = EntityService:SpawnAndAttachEntity( "misc/marker_selector/building_info", self.selector )
+        EntityService:SetPosition( self.infoChild, -1, 0, 1)
+    end
+end
+
+function lamp_base_tool:GetLampBlueprintName( selectorDB )
+
+    local defaultLamp = "buildings/decorations/base_lamp"
+
+    local parameterName = "$selected_lamp_blueprint"
+
+    local blueprintName = ""
+
+    if ( selectorDB and selectorDB:HasString(parameterName) ) then
+
+        blueprintName = selectorDB:GetStringOrDefault(parameterName, defaultLamp)
+    end
+
+    if ( blueprintName == "" ) then
+        local campaignDatabase = CampaignService:GetCampaignData()
+        if ( campaignDatabase and campaignDatabase:HasString(parameterName) ) then
+            blueprintName = campaignDatabase:GetStringOrDefault(parameterName, defaultLamp)
+        end
+    end
+
+    if ( blueprintName == "" ) then
+        return defaultLamp
+    end
+
+    if ( not ResourceManager:ResourceExists( "EntityBlueprint", blueprintName ) ) then
+        return defaultLamp
+    end
+
+    if ( not BuildingService:IsBuildingAvailable( self.playerId, blueprintName ) ) then
+        return defaultLamp
+    end
+
+    local buildingDesc = BuildingService:GetBuildingDesc( blueprintName )
+    if ( buildingDesc == nil ) then
+        return defaultLamp
+    end
+
+    local buildingDescRef = reflection_helper( buildingDesc )
+    if ( buildingDescRef == nil ) then
+        return defaultLamp
+    end
+
+    if ( buildingDescRef.build_cost == nil or buildingDescRef.build_cost.resource == nil or buildingDescRef.build_cost.resource.count == nil or buildingDescRef.build_cost.resource.count <= 0 ) then
+        return defaultLamp
+    end
+
+    return blueprintName
+end
+
+function lamp_base_tool:FindMinDistance()
+
+    self.radius = BuildingService:FindEnergyRadius( self.lampBlueprintName )
+
+    if ( self.radius == nil ) then
+        self.radius = 16
+    end
+end
+
+function lamp_base_tool:FillLampBuildingDesc(lampBlueprintName)
+
+    local buildingDesc = reflection_helper( BuildingService:GetBuildingDesc( lampBlueprintName ) )
+
+    self.ghostBlueprintName = buildingDesc.ghost_bp
+    self.buildingDesc = buildingDesc
+end
+
+function lamp_base_tool:SpawnGhostLampEntity(position, orientation)
+
+    local buildingEntity = EntityService:SpawnAndAttachEntity( self.ghostBlueprintName, self.selector )
+
+    EntityService:RemoveComponent( buildingEntity, "LuaComponent" )
+
+    EntityService:SetPosition( buildingEntity, position )
+    EntityService:SetOrientation( buildingEntity, orientation )
+    EntityService:ChangeMaterial( buildingEntity, "selector/hologram_blue" )
+
+    return buildingEntity
+end
+
+function lamp_base_tool:OnWorkExecute()
+
+    if ( self.OnUpdate ) then
+        self:OnUpdate()
+    end
+end
+
+function lamp_base_tool:CheckEntityBuildable( entity, transform, id )
+
+    id = id or 1
+    local test = nil
+
+    test = BuildingService:CheckGhostBuildingStatus( self.playerId, entity, transform, self.lampBlueprintName, id )
+
+    if ( test == nil ) then
+        return
+    end
+
+    local testBuildable = reflection_helper(test:ToTypeInstance(), test )
+
+    local canBuildOverride = (testBuildable.flag == CBF_OVERRIDES)
+    local canBuild = (testBuildable.flag == CBF_CAN_BUILD or testBuildable.flag == CBF_ONE_GRID_FLOOR or testBuildable.flag == CBF_OVERRIDES)
+
+    local skinned = EntityService:IsSkinned(entity)
+
+    if ( testBuildable.flag == CBF_REPAIR ) then
+        if ( BuildingService:CanAffordRepair( testBuildable.entity_to_repair, self.playerId, -1 )) then
+            if ( skinned ) then
+                EntityService:ChangeMaterial( entity, "selector/hologram_skinned_pass")
+            else
+                EntityService:ChangeMaterial( entity, "selector/hologram_pass")
+            end
+        else
+            if ( skinned ) then
+                EntityService:ChangeMaterial( entity, "selector/hologram_skinned_deny")
+            else
+                EntityService:ChangeMaterial( entity, "selector/hologram_deny")
+            end
+        end
+    else
+        if ( canBuildOverride ) then
+            if ( skinned ) then
+                EntityService:ChangeMaterial( entity, "selector/hologram_active_skinned")
+            else
+                EntityService:ChangeMaterial( entity, "selector/hologram_active")
+            end
+        elseif ( canBuild ) then
+            EntityService:ChangeMaterial( entity, "selector/hologram_blue")
+        else
+            EntityService:ChangeMaterial( entity, "selector/hologram_red")
+        end
+    end
+
+    return testBuildable
+end
+
+function lamp_base_tool:BuildEntity(entity, transform, createCube)
+
+    createCube = createCube or false
+
+    local testBuildable = self:CheckEntityBuildable( entity , transform )
+
+    if ( testBuildable == nil ) then
+
+        return
+    end
+
+    local missingResources = testBuildable.missing_resources
+    if ( missingResources.count > 0 ) then
+
+        local soundAnnouncement = "voice_over/announcement/not_enough_resources"
+
+        if ( missingResources.count  == 1 ) then
+
+            local singleMissingResource = missingResources[1]
+
+            if ( self.announcements and self.announcements[singleMissingResource] ~= nil and self.announcements[singleMissingResource] ~= "" ) then
+
+                soundAnnouncement = self.announcements[singleMissingResource]
+            end
+        end
+
+        QueueEvent( "PlayTimeoutSoundRequest", INVALID_ID, 5.0, soundAnnouncement, self.playerEntity, false )
+
+        return testBuildable.flag
+    end
+
+    local buildingComponent = reflection_helper( EntityService:GetComponent( entity, "BuildingComponent" ) )
+
+    if ( testBuildable.flag == CBF_CAN_BUILD ) then
+
+        self:BuildDesertFloor(transform)
+
+        QueueEvent("BuildBuildingRequest", INVALID_ID, self.playerId, buildingComponent.bp, transform, createCube )
+
+    elseif( testBuildable.flag == CBF_OVERRIDES ) then
+
+        for entityToSell in Iter(testBuildable.entities_to_sell) do
+            QueueEvent("SellBuildingRequest", entityToSell, self.playerId, false )
+        end
+
+        self:BuildDesertFloor(transform)
+
+        QueueEvent("BuildBuildingRequest", INVALID_ID, self.playerId, buildingComponent.bp, transform, createCube )
+
+    elseif( testBuildable.flag == CBF_REPAIR and testBuildable.entity_to_repair ~= nil and testBuildable.entity_to_repair ~= INVALID_ID ) then
+
+        QueueEvent( "ScheduleRepairBuildingRequest", testBuildable.entity_to_repair, self.playerId )
+    end
+
+    return testBuildable.flag
+end
+
+function lamp_base_tool:BuildDesertFloor(spot)
+
+    local antiQuickSandFloor = "buildings/decorations/floor_desert_1x1"
+
+    if ( BuildingService:IsBuildingAvailable( self.playerId, antiQuickSandFloor ) and BuildingService:CanAffordBuilding( antiQuickSandFloor, self.playerId) ) then
+
+        local buildDesertFloor = self:ShouldBuildDesertFloor( spot.position )
+
+        if ( buildDesertFloor ) then
+
+            local transformFloor = {}
+            transformFloor.position = spot.position
+            transformFloor.orientation = { x=0, y=0, z=0, w=1}
+            transformFloor.scale = { x=1, y=1, z=1}
+
+            QueueEvent("BuildFloorRequest", INVALID_ID, self.playerId, antiQuickSandFloor, transformFloor )
+        end
+    end
+end
+
+function lamp_base_tool:ShouldBuildDesertFloor( position )
+
+    local terrainType = ""
+
+    local overrideTerrains = {}
+
+    local terrainCellEntityId = EnvironmentService:GetTerrainCell(position)
+
+    if ( terrainCellEntityId ~= nil and terrainCellEntityId ~= INVALID_ID ) then
+
+        local terrainTypeLayerComponent = EntityService:GetComponent( terrainCellEntityId, "TerrainTypeLayerComponent" )
+
+        if ( terrainTypeLayerComponent ~= nil ) then
+
+            local terrainTypeLayerComponentRef = reflection_helper(terrainTypeLayerComponent)
+
+            if ( terrainTypeLayerComponentRef.terrain_type and terrainTypeLayerComponentRef.terrain_type.resource and terrainTypeLayerComponentRef.terrain_type.resource.name ) then
+
+                terrainType = terrainTypeLayerComponentRef.terrain_type.resource.name
+            end
+        end
+
+        local overrideTerrainComponent = EntityService:GetComponent( terrainCellEntityId, "OverrideTerrainComponent" )
+
+        if ( overrideTerrainComponent ~= nil ) then
+
+            local overrideTerrainComponentRef = reflection_helper(overrideTerrainComponent)
+
+            if ( overrideTerrainComponentRef.terrain_overrides ) then
+
+                for i=1,overrideTerrainComponentRef.terrain_overrides.count do
+
+                    local terrainTypeHolder = overrideTerrainComponentRef.terrain_overrides[i]
+
+                    if ( terrainTypeHolder and terrainTypeHolder.resource and terrainTypeHolder.resource.name ) then
+
+                        if ( IndexOf( overrideTerrains, terrainTypeHolder.resource.name ) == nil ) then
+                            Insert( overrideTerrains, terrainTypeHolder.resource.name )
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    local isQuickSand = (terrainType == "quicksand")
+    local hasDesertFloor = (IndexOf( overrideTerrains, "desert_floor" ) ~= nil)
+
+    if ( isQuickSand and not hasDesertFloor ) then
+
+        return true
+    end
+
+    return false
+end
+
+function lamp_base_tool:OnActivateSelectorRequest()
+end
+
+function lamp_base_tool:OnDeactivateSelectorRequest()
+end
+
+function lamp_base_tool:OnRotateSelectorRequest()
+end
+
+function lamp_base_tool:OnRelease()
+
+    if ( self.infoChild ~= nil) then
+        EntityService:RemoveEntity(self.infoChild)
+        self.infoChild = nil
+    end
+end
+
+return lamp_base_tool
---
+++ LampTools/lua/buildings/decorations/lamp_tool.lua
@@ -0,0 +1,470 @@
+local lamp_base_tool = require("lua/buildings/decorations/lamp_base_tool.lua")
+require("lua/utils/reflection.lua")
+require("lua/utils/table_utils.lua")
+require("lua/utils/string_utils.lua")
+require("lua/utils/building_utils.lua")
+
+class 'lamp_tool' ( lamp_base_tool )
+
+function lamp_tool:__init()
+    lamp_base_tool.__init(self,self)
+end
+
+function lamp_tool:OnInit()
+
+    EntityService:SetVisible( self.entity , false )
+
+    self.linesEntities = {}
+
+    self.configNameSize = "$lamp_tool_size"
+
+    self.type = self.data:GetIntOrDefault("type", 1)
+
+    self.defaultRadius = math.ceil( (self.radius - 1) / 2 )
+
+    local selectorDB = EntityService:GetDatabase( self.selector )
+
+    self.currentSize = selectorDB:GetIntOrDefault(self.configNameSize, self.defaultRadius)
+    self.currentSize = self:CheckSizeExists(self.currentSize)
+
+    self.currentMarker = nil
+    self.currentMarkerSize = 0
+    self.currentMarkerBlueprint = ""
+
+    self:SpawnGhostLampEntities()
+end
+
+function lamp_tool:CheckSizeExists( currentSize )
+
+    currentSize = currentSize or self.defaultRadius
+
+    if ( currentSize < 1) then
+        currentSize = 1
+    elseif ( currentSize > self.radius) then
+        currentSize = self.radius
+    end
+
+    return currentSize
+end
+
+function lamp_tool:SpawnGhostLampEntities()
+
+    local currentSize = self:CheckSizeExists(self.currentSize)
+
+    local currentTransform = EntityService:GetWorldTransform( self.entity )
+    local orientation = currentTransform.orientation
+
+    local newPositions = self:FindPositionsToBuildLine( currentSize )
+
+    if ( #self.linesEntities > #newPositions ) then
+
+        for i=#self.linesEntities,#newPositions + 1,-1 do
+            local lineEnt = self.linesEntities[i]
+            EntityService:RemoveEntity(lineEnt)
+            self.linesEntities[i] = nil
+        end
+
+    elseif ( #self.linesEntities < #newPositions ) then
+
+        for i=#self.linesEntities + 1 ,#newPositions do
+
+            local lineEnt = self:SpawnGhostLampEntity(newPositions[i], orientation)
+
+            Insert( self.linesEntities, lineEnt )
+        end
+    end
+
+    Assert(#self.linesEntities == #newPositions, "ERROR: something wrong with line positioning: " .. tostring(#self.linesEntities) .. "/" .. tostring(#newPositions))
+
+    for i=1,#newPositions do
+
+        local transform = {}
+
+        transform.scale = {x=1,y=1,z=1}
+        transform.orientation = orientation
+        transform.position = newPositions[i]
+
+        local lineEnt = self.linesEntities[i]
+
+        EntityService:SetPosition( lineEnt, newPositions[i])
+        EntityService:SetOrientation( lineEnt, orientation )
+    end
+
+    self.buildCost = {}
+
+    local list = BuildingService:GetBuildCosts( self.lampBlueprintName, self.playerId )
+    for resourceCost in Iter(list) do
+
+        if ( self.buildCost[resourceCost.first] == nil ) then
+            self.buildCost[resourceCost.first] = 0
+        end
+
+        self.buildCost[resourceCost.first] = self.buildCost[resourceCost.first] + ( resourceCost.second * #self.linesEntities )
+    end
+
+    self:UpdateMarker(currentSize)
+end
+
+function lamp_tool:FindPositionsToBuildLine(currentSize)
+
+    if ( self.type == 2 ) then
+
+        return self:FindPositionsType2(currentSize)
+
+    elseif ( self.type == 3 ) then
+
+        return self:FindPositionsType3(currentSize)
+    end
+
+    return self:FindPositionsType1(currentSize)
+end
+
+function lamp_tool:FindPositionsType1(currentSize)
+
+    local result = {}
+
+    local delta = currentSize * 2
+
+    local newPosition = nil
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = 0
+    newPosition.z = 0
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = delta
+    newPosition.z = delta
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = - delta
+    newPosition.z = delta
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = delta
+    newPosition.z = - delta
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = - delta
+    newPosition.z = - delta
+
+    Insert( result, newPosition )
+
+    return result
+end
+
+function lamp_tool:FindPositionsType2(currentSize)
+
+    local result = {}
+
+    local delta = currentSize * 2
+
+    local newPosition = nil
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = 0
+    newPosition.z = 0
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = 0
+    newPosition.z = delta
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = delta
+    newPosition.z = 0
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = 0
+    newPosition.z = - delta
+
+    Insert( result, newPosition )
+
+    newPosition = {}
+    newPosition.y = 0
+    newPosition.x = - delta
+    newPosition.z = 0
+
+    Insert( result, newPosition )
+
+    return result
+end
+
+function lamp_tool:FindPositionsType3(currentSize)
+
+    local result = {}
+
+    local delta = currentSize * 2
+
+    for indexX = 0,1 do
+        for indexZ = 0,1 do
+
+            local maxIndex = math.max(indexX, indexZ)
+            local totalIndex = indexX + indexZ
+
+            local newPosition = nil
+
+            newPosition = {}
+            newPosition.y = 0
+            newPosition.x = indexX * delta
+            newPosition.z = indexZ * delta
+
+            newPosition.maxIndex = maxIndex
+            newPosition.totalIndex = totalIndex
+
+            Insert( result, newPosition )
+
+            if ( indexZ ~= 0 ) then
+
+                newPosition = {}
+                newPosition.y = 0
+                newPosition.x = indexX * delta
+                newPosition.z = - indexZ * delta
+
+                newPosition.maxIndex = maxIndex
+                newPosition.totalIndex = totalIndex
+
+                Insert( result, newPosition )
+            end
+
+            if ( indexX ~= 0 ) then
+
+                newPosition = {}
+                newPosition.y = 0
+                newPosition.x = - indexX * delta
+                newPosition.z = indexZ * delta
+
+                newPosition.maxIndex = maxIndex
+                newPosition.totalIndex = totalIndex
+
+                Insert( result, newPosition )
+            end
+
+            if ( indexX ~= 0 and indexZ ~= 0 ) then
+
+                newPosition = {}
+                newPosition.y = 0
+                newPosition.x = - indexX * delta
+                newPosition.z = - indexZ * delta
+
+                newPosition.maxIndex = maxIndex
+                newPosition.totalIndex = totalIndex
+
+                Insert( result, newPosition )
+            end
+        end
+    end
+
+    local sorter = function( position1, position2 )
+
+        if (position1.maxIndex ~= position2.maxIndex) then
+
+            return position1.maxIndex < position2.maxIndex
+        end
+
+        if (position1.totalIndex ~= position2.totalIndex) then
+
+            return position1.totalIndex < position2.totalIndex
+        end
+
+        if (position1.x ~= position2.x) then
+
+            return position1.x < position2.x
+        end
+
+        return position1.z < position2.z
+    end
+
+    table.sort(result, sorter)
+
+    return result
+end
+
+function lamp_tool:UpdateMarker(currentSize)
+
+    if ( self.currentMarkerSize ~= currentSize ) then
+
+        self.currentMarkerSize = currentSize
+
+        local markerBlueprint = "misc/marker_selector_lamp_tool_radius_" .. tostring(currentSize)
+
+        if ( currentSize > 16 ) then
+            markerBlueprint = "misc/marker_selector_lamp_tool_radius_g16"
+        end
+
+        if ( self.currentMarkerBlueprint ~= markerBlueprint or self.currentMarker == nil ) then
+
+            -- Destroy old marker
+            if (self.currentMarker ~= nil) then
+
+                EntityService:RemoveEntity(self.currentMarker)
+                self.currentMarker = nil
+            end
+
+            self.currentMarkerBlueprint = markerBlueprint
+
+            self.currentMarker = EntityService:SpawnAndAttachEntity( markerBlueprint, self.selector )
+            EntityService:SetPosition( self.currentMarker, 0, 0, -2 )
+        end
+    end
+end
+
+function lamp_tool:OnUpdate()
+
+    for number=1,#self.linesEntities do
+
+        local lineEnt = self.linesEntities[number]
+
+        local transform = EntityService:GetWorldTransform( lineEnt )
+
+        local testBuildable = self:CheckEntityBuildable( lineEnt, transform, number )
+    end
+
+
+
+    self:CreateInfoChild()
+
+    local onScreen = CameraService:IsOnScreen( self.infoChild, 1 )
+
+    if ( onScreen ) then
+        BuildingService:OperateBuildCosts( self.infoChild, self.playerId, self.buildCost )
+    else
+        BuildingService:OperateBuildCosts( self.infoChild, self.playerId, {} )
+    end
+
+
+
+    if ( self.activated and self.buildPosition ~= nil ) then
+
+        local currentPosition = EntityService:GetWorldTransform( self.entity )
+
+        local spots = BuildingService:FindSpotsByDistance( self.buildPosition, currentPosition, self.radius, self.lampBlueprintName )
+
+        for spot in Iter( spots ) do
+
+            local currentSize = self:CheckSizeExists(self.currentSize)
+            local newPositions = self:FindPositionsToBuildLine( currentSize )
+
+            for i=1,#newPositions do
+
+                local newPosition = newPositions[i]
+
+                local transform = {}
+                transform.scale = spot.scale
+                transform.orientation = spot.orientation
+
+                transform.position = {}
+                transform.position.x = spot.position.x + newPosition.x
+                transform.position.y = spot.position.y + newPosition.y
+                transform.position.z = spot.position.z + newPosition.z
+
+                local lineEnt = self.linesEntities[i]
+
+                self:BuildEntity(lineEnt, transform, true)
+            end
+
+            self.buildPosition = spot
+        end
+    end
+end
+
+function lamp_tool:OnActivateSelectorRequest()
+
+    self:FinishLineBuild()
+
+    self.buildPosition = EntityService:GetWorldTransform( self.entity )
+    self.activated = true
+end
+
+function lamp_tool:OnDeactivateSelectorRequest()
+
+    self.buildPosition = nil
+    self.activated = false
+end
+
+function lamp_tool:OnRotateSelectorRequest(evt)
+
+    local degree = evt:GetDegree()
+
+    local change = 1
+    if ( degree > 0 ) then
+        change = -1
+    end
+
+    local currentSize = self:CheckSizeExists(self.currentSize)
+
+    local newValue = currentSize + change
+
+    if ( newValue < 1 ) then
+        newValue = 1
+    elseif ( newValue > self.radius ) then
+        newValue = self.radius
+    end
+
+    self.currentSize = newValue
+
+    local selectorDB = EntityService:GetDatabase( self.selector )
+    selectorDB:SetInt(self.configNameSize, newValue)
+
+    self:SpawnGhostLampEntities()
+end
+
+function lamp_tool:FinishLineBuild()
+
+    local count = #self.linesEntities
+
+    for i=1,count do
+
+        local ghostEntity = self.linesEntities[i]
+
+        local transform = EntityService:GetWorldTransform( ghostEntity )
+
+        self:BuildEntity(ghostEntity, transform, true)
+    end
+end
+
+function lamp_tool:OnRelease()
+
+    if ( self.linesEntities ~= nil) then
+        for ghostEntity in Iter(self.linesEntities) do
+            EntityService:RemoveEntity(ghostEntity)
+        end
+    end
+    self.linesEntities = {}
+
+    self.currentMarkerSize = 0
+    self.currentMarkerBlueprint = ""
+
+    if (self.currentMarker ~= nil) then
+
+        EntityService:RemoveEntity(self.currentMarker)
+        self.currentMarker = nil
+    end
+
+    if ( lamp_base_tool.OnRelease ) then
+        lamp_base_tool.OnRelease(self)
+    end
+end
+
+return lamp_tool
