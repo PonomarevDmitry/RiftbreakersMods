6b7ad5d87633db5fa96577eecc9cee24a38a4ee9
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-10-27 23:54:09 +0300

    zBuildingsResources. Chaning resource mining buildings icons.

---
+++ zBuildingsResources/lua/change_resource_buildings_icons_autoexec.lua
@@ -0,0 +1,76 @@
+require("lua/utils/reflection.lua")
+
+local InjectChangeBlueprintMinimapItemComponentIcons = function(blueprintsList)
+
+    for _,configObj in ipairs(blueprintsList) do
+
+        local icon = configObj.icon
+        local biggerIcon = configObj.bigger_icon
+
+        local list = configObj.list
+
+        for _,blueprintName in ipairs(list) do
+
+            local blueprint = ResourceManager:GetBlueprint( blueprintName )
+            if ( blueprint == nil ) then
+
+                LogService:Log("InjectChangeBlueprintMinimapItemComponentIcons Blueprint " .. blueprintName .. " NOT EXISTS.")
+                goto continue
+            end
+
+            local minimapItemComponent = blueprint:GetComponent("MinimapItemComponent")
+            if ( minimapItemComponent == nil ) then
+                
+                LogService:Log("InjectChangeBlueprintMinimapItemComponentIcons Blueprint " .. blueprintName .. " MinimapItemComponent NOT EXISTS.")
+                goto continue
+            end
+    
+            minimapItemComponent:GetField("icon_material"):SetValue(icon)
+            minimapItemComponent:GetField("type"):SetValue("1")
+            minimapItemComponent:GetField("big_minimap_scale"):SetValue("2")
+
+            local minimapItemComponentRef = reflection_helper(minimapItemComponent)
+
+            minimapItemComponentRef.color.x = 0
+            minimapItemComponentRef.color.y = 0
+            minimapItemComponentRef.color.z = 0
+            minimapItemComponentRef.color.w = 0
+
+            minimapItemComponentRef.size.x = 0.5
+            minimapItemComponentRef.size.y = 0.5
+
+            --LogService:Log("InjectChangeBlueprintMinimapItemComponentIcons Blueprint " .. blueprintName .. " minimapItemComponentRef " .. tostring(minimapItemComponentRef))
+
+            ::continue::
+        end
+    end
+end
+
+local supported_item_blueprints = {
+
+    {
+        ["icon"] = "gui/hud/minimap_icons/mining_carbonium",
+        ["list"] = {
+
+            "buildings/resources/carbonium_factory",
+        },
+    },
+
+    {
+        ["icon"] = "gui/hud/minimap_icons/mining_ironium",
+        ["list"] = {
+
+            "buildings/resources/steel_factory",
+        },
+    },
+
+    {
+        ["icon"] = "gui/hud/minimap_icons/mining_rare",
+        ["list"] = {
+    
+            "buildings/resources/rare_element_mine",
+        },
+    },
+}
+
+InjectChangeBlueprintMinimapItemComponentIcons(supported_item_blueprints)
