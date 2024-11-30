8c9d641fda20a052c2c10c05296ffded8938a5ef
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2024-11-30 13:13:13 +0300

    CustomizableTurretsCluster. Unlock research script.

--- CustomizableTurretsCluster/lua/turrets_cluster_unlock_researches_autoexec.lua
+++ CustomizableTurretsCluster/lua/turrets_cluster_unlock_researches_autoexec.lua
@@ -1,4 +1,14 @@
 require("lua/utils/reflection.lua")
+require("lua/utils/table_utils.lua")
+
+local supported_research_list = {
+
+    {
+        ["name"] = "gui/menu/research/name/turrets_cluster_mods_scanner_turret_standard_item",
+        ["research_category"] = "gui/menu/research/category_weapons_equipment",
+        ["research_name"] = "gui/menu/research/name/consumable_scanner_turret_standard",
+    },
+}
 
 local turrets_cluster_unlock_researches_autoexec = function(evt)
 
@@ -9,8 +19,27 @@ local turrets_cluster_unlock_researches_autoexec = function(evt)
 
     local researchComponentRef = reflection_helper( researchComponent )
 
-    --PlayerService:UnhideResearch( "gui/menu/research/name/turrets_cluster_mods_scanner_turret_standard_item" )
-    --PlayerService:EnableResearch( "gui/menu/research/name/turrets_cluster_mods_scanner_turret_standard_item" )
+    local categories = researchComponentRef.research
+
+    for i=1,categories.count do
+
+        local category = categories[i]
+
+        local category_nodes = category.nodes
+        for j=1,category_nodes.count do
+
+            local node = category_nodes[j]
+
+            for _, item in ipairs(supported_research_list) do
+
+                if ( node.research_name == item.research_name and category.category == item.research_category ) then
+
+                    PlayerService:UnhideResearch( item.name )
+                    PlayerService:EnableResearch( item.name )
+                end
+            end
+        end
+    end
 end
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
