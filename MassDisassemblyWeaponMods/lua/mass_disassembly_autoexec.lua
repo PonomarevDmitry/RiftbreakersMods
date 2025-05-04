c37ff37bac90fffb3529fc2056a09d644d997474
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-05-04 18:29:59 +0300

    MassDisassemblyWeaponMods. Adding tool to disassembly weapon mods.

--- MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
+++ MassDisassemblyWeaponMods/lua/mass_disassembly_autoexec.lua
@@ -5,6 +5,8 @@ local mass_disassembly_autoexec = function(evt)
         BuildingService:UnlockBuilding("buildings/main/mass_disassembly")
         BuildingService:UnlockBuilding("buildings/main/mass_disassembly_by_rarity")
         BuildingService:UnlockBuilding("buildings/main/mass_disassembly_equal_and_lower")
+        
+        BuildingService:UnlockBuilding("buildings/tools/mass_disassembly_by_rarity_tool")
     end
 
 
---
+++ MassDisassemblyWeaponMods/lua/misc/mass_disassembly_by_rarity_tool.lua
@@ -0,0 +1,59 @@
+local tool = require("lua/misc/tool.lua")
+require("lua/utils/table_utils.lua")
+
+class 'mass_disassembly_by_rarity_tool' ( tool )
+
+function mass_disassembly_by_rarity_tool:__init()
+    tool.__init(self,self)
+end
+
+function mass_disassembly_by_rarity_tool:OnInit()
+
+    self.childEntity = EntityService:SpawnAndAttachEntity("misc/marker_selector_mass_disassembly_by_rarity_tool", self.entity)
+
+    self.player = PlayerService:GetPlayerControlledEnt(self.playerId)
+
+    self.scaleMap = {
+        1,
+    }
+end
+
+function mass_disassembly_by_rarity_tool:GetScaleFromDatabase()
+    return { x=1, y=1, z=1 }
+end
+
+function mass_disassembly_by_rarity_tool:OnPreInit()
+    self.initialScale = { x=1, y=1, z=1 }
+end
+
+function mass_disassembly_by_rarity_tool:SpawnCornerBlueprint()
+    if ( self.corners == nil ) then
+        self.corners = EntityService:SpawnAndAttachEntity("misc/marker_selector_corner_tool", self.entity )
+    end
+end
+
+function mass_disassembly_by_rarity_tool:FindEntitiesToSelect( selectorComponent )
+
+    return {}
+end
+
+function mass_disassembly_by_rarity_tool:OnUpdate()
+end
+
+function mass_disassembly_by_rarity_tool:AddedToSelection( entity )
+end
+
+function mass_disassembly_by_rarity_tool:RemovedFromSelection( entity )
+end
+
+function mass_disassembly_by_rarity_tool:OnRotate()
+end
+
+function mass_disassembly_by_rarity_tool:OnActivateSelectorRequest()
+end
+
+
+function mass_disassembly_by_rarity_tool:OnGuiPopupResultEvent( evt )
+end
+
+return mass_disassembly_by_rarity_tool
