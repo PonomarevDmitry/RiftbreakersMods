0628b688ef4b346b9d99b9d9fee6696954ee45b6
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-05-08 23:45:08 +0300

    zTestMod. Changing mech deacceleration value.

--- zTestMod/lua/mech_changes_movementdata_autoexec.lua
+++ zTestMod/lua/mech_changes_movementdata_autoexec.lua
@@ -38,7 +38,7 @@ local InjectChangeMechMovementDataValues = function(blueprintName, acceleration,
 end
 
 local accelerationValue = "100"
-local deaccelerationValue = "150"
+local deaccelerationValue = "200"
 
 InjectChangeMechMovementDataValues("player/character_base", accelerationValue, deaccelerationValue)
 
