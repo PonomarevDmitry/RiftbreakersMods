ed7faae11b0f2334caaff223d03fc77842b4eb89
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-05-18 16:31:15 +0300

    WallGateReplacementTools. Changing menu message.

--- WallGateReplacementTools/lua/misc/replace_wall_gate_picker_tool.lua
+++ WallGateReplacementTools/lua/misc/replace_wall_gate_picker_tool.lua
@@ -92,7 +92,7 @@ function replace_wall_gate_picker_tool:SetBuildingIcon()
             buildingIcon = menuIcon
             buildingIconVisible = 1
 
-            messageText = "${gui/hud/messages/replace_wall_gate_tool/current_building} ${" .. buildingDescRef.localization_id .. "}"
+            messageText = "${gui/hud/messages/replace_wall_gate_tool/current_building}:\n${" .. buildingDescRef.localization_id .. "}"
         end
     end
 
