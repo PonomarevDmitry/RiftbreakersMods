ed798ee05b6baba2ec9ac79029184f50b668f9c8
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-05-25 18:48:02 +0300

    MinesEraser. Hightlight selected entities.

--- MinesEraser/lua/misc/eraser_mines_tool.lua
+++ MinesEraser/lua/misc/eraser_mines_tool.lua
@@ -105,9 +105,17 @@ function eraser_mines_tool:FindEntitiesToSelect( selectorComponent )
 end
 
 function eraser_mines_tool:AddedToSelection( entity )
+
+    local skinned = EntityService:IsSkinned(entity)
+    if ( skinned ) then
+        EntityService:SetMaterial( entity, "selector/hologram_current_skinned", "selected" )
+    else
+        EntityService:SetMaterial( entity, "selector/hologram_current", "selected" )
+    end
 end
 
 function eraser_mines_tool:RemovedFromSelection( entity )
+    EntityService:RemoveMaterial( entity, "selected" )
 end
 
 function eraser_mines_tool:OnRotate()
