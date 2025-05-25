94d78934b77172a3779d5a5b0c4ee0152ba543b7
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-05-25 14:51:09 +0300

    zDroneChange. Scripg refactoring.

--- zDroneChange/lua/zdronechanges_changes_movementdata_autoexec.lua
+++ zDroneChange/lua/zdronechanges_changes_movementdata_autoexec.lua
@@ -23,6 +23,8 @@ local InjectChangeDronesMovementDataValues = function(blueprintName, acceleratio
         movementDataComponentAcceleration:SetRealSelfAwareType()
 
         movementDataComponentAcceleration:GetField('base'):SetValue(acceleration)
+
+        --LogService:Log("InjectChangeDronesMovementDataValues Setting Blueprint '" .. blueprintName .. "' movementDataComponent:GetField('acceleration') = " .. tostring(acceleration))
     end
 
     local movementDataComponentDeacceleration = movementDataComponent:GetField("deacceleration")
@@ -34,6 +36,8 @@ local InjectChangeDronesMovementDataValues = function(blueprintName, acceleratio
         movementDataComponentDeacceleration:SetRealSelfAwareType()
 
         movementDataComponentDeacceleration:GetField('base'):SetValue(deacceleration)
+
+        --LogService:Log("InjectChangeDronesMovementDataValues Setting Blueprint '" .. blueprintName .. "' movementDataComponent:GetField('deacceleration') = " .. tostring(deacceleration))
     end
 
     local movementDataComponentMaxSpeed = movementDataComponent:GetField("max_speed")
@@ -45,6 +49,8 @@ local InjectChangeDronesMovementDataValues = function(blueprintName, acceleratio
         movementDataComponentMaxSpeed:SetRealSelfAwareType()
 
         movementDataComponentMaxSpeed:GetField('base'):SetValue(maxSpeed)
+
+        --LogService:Log("InjectChangeDronesMovementDataValues Setting Blueprint '" .. blueprintName .. "' movementDataComponent:GetField('max_speed') = " .. tostring(maxSpeed))
     end
 end
 
@@ -52,134 +58,46 @@ local InjectChangeListDronesMovementDataValues = function(blueprintStorageValues
 
     for _, configObject in ipairs(blueprintStorageValues) do
 
-        InjectChangeDronesMovementDataValues(configObject.name, configObject.acceleration, configObject.deacceleration, configObject.max_speed)
+        if ( configObject.name ~= nil and configObject.name ~= "" ) then
+            InjectChangeDronesMovementDataValues(configObject.name, configObject.acceleration, configObject.deacceleration, configObject.max_speed)
+        end
+
+        if ( configObject.list ~= nil ) then
+
+            for _, blueprintName in ipairs(configObject.list) do
+
+                InjectChangeDronesMovementDataValues(blueprintName, configObject.acceleration, configObject.deacceleration, configObject.max_speed)
+            end
+        end
     end
 end
 
 local new_values = {
 
     {
-        ["name"] = "units/drones/drone_flora_collector",
+        ["list"] = {
+            "units/drones/drone_flora_collector",
+            "units/drones/drone_resource_collector",
 
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
+            "units/drones/drone_repair_facility_base",
 
-    {
-        ["name"] = "units/drones/drone_resource_collector",
+            "units/drones/drone_repair_facility_lvl_1",
+            "units/drones/drone_repair_facility_lvl_2",
+            "units/drones/drone_repair_facility_lvl_3",
 
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
+            "units/drones/drone_player_repair_standard",
+            "units/drones/drone_player_repair_advanced",
+            "units/drones/drone_player_repair_superior",
+            "units/drones/drone_player_repair_extreme",
 
-    {
-        ["name"] = "units/drones/drone_repair_facility_base",
+            "units/drones/drone_player_loot_collector_standard",
+            "units/drones/drone_player_loot_collector_advanced",
+            "units/drones/drone_player_loot_collector_superior",
+            "units/drones/drone_player_loot_collector_extreme",
 
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_repair_facility_lvl_1",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_repair_facility_lvl_2",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_repair_facility_lvl_3",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_repair_standard",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_repair_advanced",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_repair_superior",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_repair_extreme",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_loot_collector_standard",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_loot_collector_advanced",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_loot_collector_superior",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_player_loot_collector_extreme",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_loot_collector",
-
-        ["acceleration"] = "100",
-        ["deacceleration"] = "100",
-        ["max_speed"] = "250",
-    },
-
-    {
-        ["name"] = "units/drones/drone_flora_cultivator",
+            "units/drones/drone_loot_collector",
+            "units/drones/drone_flora_cultivator",
+        },
 
         ["acceleration"] = "100",
         ["deacceleration"] = "100",
