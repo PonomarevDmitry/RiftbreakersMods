b837d2ff68fbea8074008876b8c53f342049a859
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-06-03 20:23:03 +0300

    zBuildingsDefense. Changing gatling laser ghost display radius component.

--- zBuildingsDefense/lua/entities_defense_changes_towers_autoexec.lua
+++ zBuildingsDefense/lua/entities_defense_changes_towers_autoexec.lua
@@ -50,3 +50,56 @@ local new_values = {
 }
 
 InjectChangeListTowersValues(new_values)
+
+
+
+local InjectChangeTowersGhostValues = function(blueprintName, rangeMax, aimingRange)
+
+    blueprintName = blueprintName .. "_ghost"
+
+    local blueprint = ResourceManager:GetBlueprint( blueprintName )
+    if ( blueprint == nil ) then
+        LogService:Log("InjectChangeTowersValues Blueprint '" .. blueprintName .. "' NOT EXISTS.")
+        return
+    end
+
+    local displayRadiusComponent = blueprint:GetComponent("DisplayRadiusComponent")
+    if ( displayRadiusComponent == nil ) then
+        LogService:Log("InjectChangeTowersValues Blueprint '" .. blueprintName .. "' DisplayRadiusComponent NOT EXISTS.")
+        return
+    end
+
+    local max_radiusField = displayRadiusComponent:GetField("max_radius")
+    if ( max_radiusField == nil ) then
+        LogService:Log("InjectChangeTowersValues Blueprint '" .. blueprintName .. "' displayRadiusComponent:GetField('max_radius') NOT EXISTS.")
+        return
+    end
+
+    max_radiusField:SetValue(rangeMax)
+end
+
+local new_ghost_values = {
+
+    {
+        ["list"] = { 
+            "buildings/defense/tower_gatling_laser",
+            "buildings/defense/tower_gatling_laser_lvl_2",
+            "buildings/defense/tower_gatling_laser_lvl_3"
+        },
+
+        ["range_max"] = "60",
+    },
+}
+
+local InjectChangeListTowersGhostValues = function(blueprintStorageValues)
+
+    for _, configObject in ipairs(blueprintStorageValues) do
+
+        for _, blueprintName in ipairs(configObject.list) do
+
+            InjectChangeTowersGhostValues(blueprintName, configObject.range_max)
+        end
+    end
+end
+
+InjectChangeListTowersGhostValues(new_ghost_values)
