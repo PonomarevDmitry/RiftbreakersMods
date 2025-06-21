6085dffa79836a8206b213e8af99001523d3db82
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-06-21 19:31:33 +0300

    Coop. Updating base_unit.lua script to coop update.

--- ShowUnitVulnerabilities/lua/units/base_unit.lua
+++ ShowUnitVulnerabilities/lua/units/base_unit.lua
@@ -16,6 +16,10 @@ function base_unit:init()
 	SetupUnitScale( self.entity, self.data )
 
 	self.lastDamageGenericTime = 0
+	self.normalExplodeProbability = 1
+	self.leaveBodyProbability = 5
+	self.wreckMinSpeed = 8
+	self.disallowDeathAnim = ""
 
 	self:RegisterHandler( self.entity, "DestroyRequest",  "_OnDestroyRequest" )
 	self:RegisterHandler( self.entity, "DamageEvent",  "_OnDamageEvent" )
@@ -33,6 +37,10 @@ end
 function base_unit:OnInit()
 end
 
+function base_unit:CreateNormalExplode()
+	EntityService:RequestDestroyPattern( self.entity, "wreck" )
+end
+
 function base_unit:_OnDamageEvent( evt )
 	-- `damage_generic` spawning moved to FxSystem so it is done only on client side!
 	if not evt:GetDamageOverTime() then 
@@ -41,6 +49,27 @@ function base_unit:_OnDamageEvent( evt )
 
 	if self.OnDamageEvent then
 		self:OnDamageEvent(evt)
+	end
+
+	local damageType = evt:GetDamageType()
+
+	if ( ( UnitService:IsOnHeightGround( self.entity ) == true ) or ( damageType == 'gravity' ) ) then
+		self:CreateNormalExplode()
+	else
+		local BehaviorRange1 = 0 + self.normalExplodeProbability
+		local BehaviorRange2 = BehaviorRange1 + self.leaveBodyProbability	
+		local probabilitySum = self.normalExplodeProbability + self.leaveBodyProbability    
+		local behaviorNumber = RandInt( 0, probabilitySum )
+
+		--LogService:Log( "base_unit : " .. tostring( self.normalExplodeProbability) .. " " .. tostring( self.leaveBodyProbability ) ) 
+
+		UnitService:SetRandomDeathAnimation( self.entity, self.wreckMinSpeed, "death", self.disallowDeathAnim )
+
+		if ( behaviorNumber >= 0 and behaviorNumber <= BehaviorRange1 and BehaviorRange1 > 0 ) then
+			self:CreateNormalExplode()
+		elseif (behaviorNumber > BehaviorRange1 or BehaviorRange1 == 0) and behaviorNumber <= BehaviorRange2  then
+			EntityService:ChangeToWreck( self.entity, evt:GetDamageType(), evt:GetDamagePercentage(), self.wreck_type, self.wreckMinSpeed )
+		end	
 	end
 end
 
@@ -294,6 +323,15 @@ function base_unit:OnLoad()
 		end
 
 		self.soundFSM = nil
+	end
+	
+	if ( self.normalExplodeProbability == nil ) or ( self.leaveBodyProbability == nil ) then
+		self.normalExplodeProbability = 1
+		self.leaveBodyProbability = 0
+	end
+
+	if ( self.disallowDeathAnim == nil ) then
+		self.disallowDeathAnim = ""
 	end
 	
 	self.baseUnitVersion = CURRENT_BASE_UNIT_VERSION
