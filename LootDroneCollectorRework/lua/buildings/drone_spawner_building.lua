cf878c5ab61f24a7a7a539103b8c35b2c241b5b3
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-08-27 22:04:29 +0300

    LootDroneCollectorRework. Ignoring point entity in Move Drone Area Tool.

--- LootDroneCollectorRework/lua/buildings/drone_spawner_building.lua
+++ LootDroneCollectorRework/lua/buildings/drone_spawner_building.lua
@@ -508,6 +508,9 @@ function drone_spawner_building:CreateLinkEntity()
 
 		local team = EntityService:GetTeam( self.entity )
 		self.linkEntity = EntityService:SpawnAndAttachEntity( linkEntityBlueprintName, self.entity, team)
+
+		EntityService:CreateComponent(self.linkEntity, "EffectReferenceComponent")
+
 		ItemService:SetInvisible(self.linkEntity, true)
 	end
 
