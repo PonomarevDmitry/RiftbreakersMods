6335989f0f4f72069acc59b3e319ffa5beafc0c1
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-08-29 19:27:59 +0300

    Weaponized Tower. Game new update.

--- Weaponized Tower/lua/tower_player_weapon.lua
+++ Weaponized Tower/lua/tower_player_weapon.lua
@@ -3,52 +3,53 @@ local tower = require("lua/buildings/defense/tower.lua")
 class 'tower_player_weapon' ( tower )
 
 TurretStatus = {
-    TS_UNKNOWN = 0,
-    TS_ON_WEAPON_AIM = 4
+	TS_UNKNOWN = 0,
+	TS_ON_WEAPON_AIM = 4
 }
 
 function tower_player_weapon:__init()
-    tower.__init(self,self)
+	tower.__init(self,self)
 end
 
 function tower_player_weapon:OnInit()
-    tower.OnInit(self)
+	tower.OnInit(self)
 
-    self.equipped_weapon = INVALID_ID
-    self.equipped_weapon_item = INVALID_ID
+	self.equipped_weapon = INVALID_ID
+	self.equipped_weapon_item = INVALID_ID
 
-    self.missing_resources = {}
-    self.attack_controller = self:CreateStateMachine()
-    self.attack_controller:AddState("shoot", { enter = "OnShootEnter", execute = "OnShootExecute", exit = "OnShootExit"})
+	self.missing_resources = {}
+	self.attack_controller = self:CreateStateMachine()
+	self.attack_controller:AddState("shoot", { enter = "OnShootEnter", execute = "OnShootExecute", exit = "OnShootExit", interval=0.1})
 
-    self.icons_controller = self:CreateStateMachine()
-    self.icons_controller:AddState("check_icons", { execute = "OnCheckIconsExecute", interval = 0.5 })
-    self.icons_controller:ChangeState("check_icons")
+	self.icons_controller = self:CreateStateMachine()
+	self.icons_controller:AddState("check_icons", { execute = "OnCheckIconsExecute", interval = 0.5 })
+	self.icons_controller:ChangeState("check_icons")
 
-    self:RegisterHandler( self.entity, "ItemEquippedEvent", "OnItemEquippedEvent" )
-    self:RegisterHandler( self.entity, "ItemUnequippedEvent", "OnItemUnequippedEvent" )
-    
+	self:RegisterHandler( self.entity, "ItemEquippedEvent", "OnItemEquippedEvent" )
+	self:RegisterHandler( self.entity, "ItemUnequippedEvent", "OnItemUnequippedEvent" )
+	self:RegisterHandler( self.entity, "TurretEvent", "OnTurretEvent" )
+
     self:RegisterHandler( self.entity, "DestroyRequest",  "OnDestroyRequest" )
 end
 
 function tower_player_weapon:OnTurretEvent( evt )
-    if self.equipped_weapon == INVALID_ID then
-        return 
-    end
+	if self.equipped_weapon == INVALID_ID then
+		return 
+	end
 
-    local status = evt:GetTurretStatus()
-    if status == TurretStatus.TS_ON_WEAPON_AIM then
-        self.attack_controller:ChangeState("shoot")
-    else
-        self.attack_controller:Deactivate();
-    end
+	local status = evt:GetTurretStatus()
+	if status == TurretStatus.TS_ON_WEAPON_AIM then
+		self.attack_controller:ChangeState("shoot")
+	else
+		self.attack_controller:Deactivate();
+	end
 end
 
 local function GetSubSlotForItem( owner, slot_name, item )
     local equipment = reflection_helper( EntityService:GetComponent(owner, "EquipmentComponent") ).equipment[1]
-    if equipment.id then
-        equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
-    end
+	if equipment.id then
+		equipment = reflection_helper( EntityService:GetComponent(equipment.id, "EquipmentComponent") ).equipment[1]
+	end
 
     local slots = equipment.slots
     for i=1,slots.count do
@@ -61,11 +62,11 @@ local function GetSubSlotForItem( owner, slot_name, item )
 
                 local entities = slot.subslots[j]
 
-                local entity = entities[1]
+				local entity = entities[1]
                 if entity and entity.id == item then
                     return j - 1
-                end
-            end
+				end
+			end
         end
     end
 
@@ -73,99 +74,101 @@ local function GetSubSlotForItem( owner, slot_name, item )
 end
 
 function tower_player_weapon:OnCheckIconsExecute()
-    if WeaponService:HasAmmoToShoot(self.equipped_weapon_item) then
-        self:ClearMissingResources()
-    end
+	if WeaponService:HasAmmoToShoot(self.equipped_weapon_item) then
+		self:ClearMissingResources()
+	end
 end
 
 function tower_player_weapon:OnResourceMissingEvent(evt)
-    local iconEntity = self.missing_resources[ evt:GetResource() ]
-    if iconEntity ~= nil then
-        return
-    end
+	local iconEntity = self.missing_resources[ evt:GetResource() ]
+	if iconEntity ~= nil then
+		return
+	end
 
-    self.missing_resources[ evt:GetResource() ] = BuildingService:AttachMissingResourceIcon( self.entity, evt:GetResource(), true)
+	self.missing_resources[ evt:GetResource() ] = BuildingService:AttachMissingResourceIcon( self.entity, evt:GetResource(), true)
 end
 
 function tower_player_weapon:ClearMissingResources()
-    for _,iconEntity in pairs(self.missing_resources) do
-        EntityService:RemoveEntity(iconEntity)
-    end
+	for _,iconEntity in pairs(self.missing_resources) do
+		EntityService:RemoveEntity(iconEntity)
+	end
 
-    self.missing_resources = {}
+	self.missing_resources = {}
 end
 
 function tower_player_weapon:OnItemUnequippedEvent( evt )
-    if self.equipped_weapon ~= evt:GetItem() then
-        return
-    end
+	if self.equipped_weapon ~= evt:GetItem() then
+		return
+	end
 
-    if self.equipped_weapon_item == INVALID_ID then
-        return
-    end
+	if self.equipped_weapon_item == INVALID_ID then
+		return
+	end
 
-    self:ClearMissingResources()
+	self:ClearMissingResources()
 
-    self:UnregisterHandler( self.equipped_weapon_item, "ResourceMissingEvent", "OnResourceMissingEvent" )
+	self:UnregisterHandler( self.equipped_weapon_item, "ResourceMissingEvent", "OnResourceMissingEvent" )
 
-    self.equipped_weapon = INVALID_ID
-    self.equipped_weapon_item = INVALID_ID
+	self.equipped_weapon = INVALID_ID
+	self.equipped_weapon_item = INVALID_ID
 end
 
 function tower_player_weapon:OnItemEquippedEvent( evt )
-    self.equipped_weapon = evt:GetItem()
+	self.equipped_weapon = evt:GetItem()
 
-    if self.equipped_weapon ~= INVALID_ID then
-        EffectService:AttachEffects(self.entity, "working")
-    else
-        EffectService:DestroyEffectsByGroup(self.entity, "working")
-    end
+	if self.equipped_weapon ~= INVALID_ID then
+		EffectService:AttachEffects(self.entity, "working")
+	else
+		EffectService:DestroyEffectsByGroup(self.entity, "working")
+	end
 
-    self.is_charge_weapon = false
+	self.is_charge_weapon = false
 
-    self.equipped_weapon_item = EntityService:GetChildByName(self.equipped_weapon, evt:GetSlot())
-    if self.equipped_weapon_item ~= INVALID_ID then
-        self.is_charge_weapon = EntityService:HasComponent(self.equipped_weapon_item,"ChargeWeaponComponent")
+	self.equipped_weapon_item = EntityService:GetChildByName(self.equipped_weapon, evt:GetSlot())
+	if self.equipped_weapon_item ~= INVALID_ID then
+		self.is_charge_weapon = EntityService:HasComponent(self.equipped_weapon_item,"ChargeWeaponComponent")
 
-        self:RegisterHandler( self.equipped_weapon_item, "ResourceMissingEvent", "OnResourceMissingEvent" )
-    end
+		self:RegisterHandler( self.equipped_weapon_item, "ResourceMissingEvent", "OnResourceMissingEvent" )
+	end
 
-    -- FIX ME: there is a bug that doesn't remove weapon from player mech so we need to do that here! 
-    local player = PlayerService:GetPlayerControlledEnt(0)
-    if player == INVALID_ID then
-        return
-    end
+	-- FIX ME: there is a bug that doesn't remove weapon from player mech so we need to do that here! 
+	local player = PlayerService:GetPlayerControlledEnt(0)
+	if player == INVALID_ID then
+		return
+	end
 
-    for slot_name in Iter({ "LEFT_HAND", "RIGHT_HAND"}) do
-        local index = GetSubSlotForItem(player, slot_name, self.equipped_weapon)
-        if index ~= -1 then
-            self:RegisterHandler( player, "ItemUnequippedEvent", "OnPlayerItemUnequippedEvent" )
-            QueueEvent("EquipmentChangeRequest", player, slot_name, index, INVALID_ID )
-        end
-    end
+	for slot_name in Iter({ "LEFT_HAND", "RIGHT_HAND"}) do
+		local index = GetSubSlotForItem(player, slot_name, self.equipped_weapon)
+		if index ~= -1 then
+			self:RegisterHandler( player, "ItemUnequippedEvent", "OnPlayerItemUnequippedEvent" )
+			QueueEvent("EquipmentChangeRequest", player, slot_name, index, INVALID_ID )
+		end
+	end
 end
 
 function tower_player_weapon:OnPlayerItemUnequippedEvent(evt)
-    self:UnregisterHandler( evt:GetEntity(), "ItemUnequippedEvent", "OnPlayerItemUnequippedEvent" )
+	self:UnregisterHandler( evt:GetEntity(), "ItemUnequippedEvent", "OnPlayerItemUnequippedEvent" )
 
-    QueueEvent("EquipItemRequest", self.entity, self.equipped_weapon, "MOD_1")
+	QueueEvent("EquipItemRequest", self.entity, self.equipped_weapon, "MOD_1")
 end
 
 function tower_player_weapon:OnShootEnter()
-    --QueueEvent("ActivateEquipmentSlotRequest", self.entity, "MOD_1", "" )
+	--QueueEvent("ActivateEquipmentSlotRequest", self.entity, "MOD_1", "" )
 end
 
 function tower_player_weapon:OnShootExecute()
-    if self.is_charge_weapon then
-        QueueEvent("ActivateOnceEquipmentSlotRequest", self.entity, "MOD_1", "" )
-    else
-        QueueEvent("DeactivateEquipmentSlotRequest", self.entity, "MOD_1", true )
-        QueueEvent("ActivateEquipmentSlotRequest", self.entity, "MOD_1", "" )
-    end
+	self.activation_id = self.activation_id or 0 + 1
+
+	if self.is_charge_weapon then
+		QueueEvent("ActivateOnceEquipmentSlotRequest", self.entity, "MOD_1", "", self.activation_id )
+	else
+		QueueEvent("DeactivateEquipmentSlotRequest", self.entity, "MOD_1", true )
+		QueueEvent("ActivateEquipmentSlotRequest", self.entity, "MOD_1", "", self.activation_id )
+	end
 end
 
 function tower_player_weapon:OnShootExit()
-    QueueEvent("DeactivateEquipmentSlotRequest", self.entity, "MOD_1", true )
+	QueueEvent("DeactivateEquipmentSlotRequest", self.entity, "MOD_1", true )
 end
 
 function tower_player_weapon:OnDestroyRequest()
