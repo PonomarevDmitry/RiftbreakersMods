4b8838c7fb29b74a1158d8aab7561bd42c925c56
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-09-13 15:16:58 +0300

    HQMoveTool. Clear HQ build consts before selling.

--- HQMoveTool/lua/hq_move_tool_event_autoexec.lua
+++ HQMoveTool/lua/hq_move_tool_event_autoexec.lua
@@ -20,6 +20,27 @@ RegisterGlobalEventHandler("OperateActionMapperRequest", function(evt)
 
     local entity = evt:GetEntity()
 
+    -- Clear Build Costs
+    local component = EntityService:GetComponent( entity, "BuildingComponent" )
+    if ( component ~= nil ) then
+
+        local componentRef = reflection_helper( component )
+
+        if ( componentRef.build_costs ~= nil and componentRef.build_costs.resource ~= nil ) then
+
+            local container = rawget( componentRef.build_costs.resource, "__ptr" );
+
+            if ( container ~= nil ) then
+
+                local itemsCount = container:GetItemCount()
+
+                for i=itemsCount,1,-1 do
+                    container:EraseItem(i-1)
+                end
+            end
+        end
+    end
+
     BuildingService:EnableSellOption( entity )
 
 end)
