29f5c31982495b8ae34911916ec53012a45998b6
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-09-17 00:36:30 +0300

    ActivateAllBioAnomaliesSkill. Disabling auto adding item to players inventory.

--- ActivateAllBioAnomaliesSkill/lua/activate_all_bioanomalies_autoexec.lua
+++ ActivateAllBioAnomaliesSkill/lua/activate_all_bioanomalies_autoexec.lua
@@ -49,11 +49,11 @@ local activate_all_bioanomalies_autoexec = function(evt)
         end
     end
 
-    local itemCount = PlayerService:GetItemNumber( playerId, skillName )
-
-    if ( itemCount == 0 ) then
-        PlayerService:AddItemToInventory( playerId, skillName )
-    end
+    --local itemCount = PlayerService:GetItemNumber( playerId, skillName )
+    --
+    --if ( itemCount == 0 ) then
+    --    PlayerService:AddItemToInventory( playerId, skillName )
+    --end
 end
 
 --RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
