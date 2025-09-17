4d241468309d8983a5999d96cd72dcb802054d4b
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-09-17 09:36:43 +0300

    zUnitTypeChanger. Updating scripts to coop.

--- zUnitTypeChanger/lua/misc/unit_type_remove_invisible_tool.lua
+++ zUnitTypeChanger/lua/misc/unit_type_remove_invisible_tool.lua
@@ -125,7 +125,9 @@ function unit_type_remove_invisible_tool:RemovedFromSelection( entity )
     EntityService:RemoveMaterial( entity, "selected" )
     local children = EntityService:GetChildren( entity, true )
     for child in Iter( children ) do
-        EntityService:RemoveMaterial( child, "selected" )
+        if ( EntityService:HasComponent( child, "MeshComponent" ) and EntityService:HasComponent( child, "HealthComponent" ) and not EntityService:HasComponent( child, "EffectReferenceComponent" ) ) then
+            EntityService:RemoveMaterial( child, "selected" )
+        end
     end
 end
 
