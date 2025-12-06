eb2311aee4bce91be59837196f9a380e2a0105bd
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-12-06 22:02:52 +0300

    AttackDronesTowersRework. Handling UnequipItemRequest.

--- AttackDronesTowersRework/lua/buildings/defense/tower_drone_attack_with_slots.lua
+++ AttackDronesTowersRework/lua/buildings/defense/tower_drone_attack_with_slots.lua
@@ -69,6 +69,8 @@ function tower_drone_attack_with_slots:RegisterEventHandlers()
     self:RegisterHandler( self.entity, "UnequipedItemEvent", "OnItemUnequippedEvent" )
     self:RegisterHandler( self.entity, "UnequipItemRequest", "OnItemUnequippedEvent" )
 
+    self:RegisterHandler( self.entity, "TimerElapsedEvent", "OnTimerElapsedEvent")
+
     self:RegisterHandler( event_sink, "EnterBuildMenuEvent", "OnEnterBuildMenuEvent" )
     self:RegisterHandler( event_sink, "EnterFighterModeEvent", "OnEnterFighterModeEvent" )
 end
@@ -213,6 +215,15 @@ function tower_drone_attack_with_slots:OnItemUnequippedEvent( evt )
 
     local database = EntityService:GetOrCreateDatabase( self.entity )
     database:SetString(key, "")
+
+    self:PopulateSpecialActionInfo()
+
+    EntityService:CreateComponent( self.entity, "TimerComponent")
+
+    QueueEvent( "SetTimerRequest", self.entity, "RefreshIcons", 3 )
+end
+
+function tower_drone_attack_with_slots:OnTimerElapsedEvent( evt )
 
     self:PopulateSpecialActionInfo()
 end
