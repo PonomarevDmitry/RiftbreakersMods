7e03948c7064817a54a941fc38a6fc80f4a3e81b
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2025-12-16 21:18:18 +0300

    EnergyGlobalizationUpdate. Updating script to game new update.

--- EnergyGlobalizationUpdate/lua/buildings/main/rift_station.lua
+++ EnergyGlobalizationUpdate/lua/buildings/main/rift_station.lua
@@ -281,7 +281,8 @@ function rift_station:OnLoad()
 		EntityService:RemoveComponent( self.entity, "SoundComponent")
 	end
 	
-	self.resourceAmount 	= self.data:GetFloatOrDefault("resource_amount", 5000)
+	self.resetCounter = self.resetCounter or 0
+	self.resourceAmount = self.data:GetFloatOrDefault("resource_amount", 5000)
 	self.riftVersion = 3
 end
 
