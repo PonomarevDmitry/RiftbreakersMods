33d11a2eff4902214a87a896904bbbe6831e5670
Ponomarev Dmitry <ponomaryow.dmitry@gmail.com>
2026-04-12 20:44:36 +0300

    MassDisassemblyWeaponMods. Adding rarity items to player's inventory.

--- MassDisassemblyWeaponMods/lua/mass_disassembly_server_autoexec.lua
+++ MassDisassemblyWeaponMods/lua/mass_disassembly_server_autoexec.lua
@@ -1,25 +1,8 @@
+require("lua/utils/reflection.lua")
 require("lua/utils/table_utils.lua")
 
 if ( not is_server ) then
     return
-end
-
-mod_autoadding_player_inventory_list = mod_autoadding_player_inventory_list or {}
-
-local itemList = {
-
-    "items/mass_disassembly_rarity/standard",
-    "items/mass_disassembly_rarity/advanced",
-    "items/mass_disassembly_rarity/superior",
-    "items/mass_disassembly_rarity/extreme"
-}
-
-for itemName in Iter( itemList ) do
-
-    if ( IndexOf( mod_autoadding_player_inventory_list, itemName ) == nil ) then
-
-        Insert(mod_autoadding_player_inventory_list, itemName)
-    end
 end
 
 
@@ -31,15 +14,98 @@ local mass_disassembly_autoexec = function(evt)
     end
 
     local buildingSystemCampaignInfoComponent = EntityService:GetSingletonComponent("BuildingSystemCampaignInfoComponent")
-    if ( buildingSystemCampaignInfoComponent == nil ) then
+    if ( buildingSystemCampaignInfoComponent ~= nil ) then
+
+        BuildingService:UnlockBuilding("buildings/main/mass_disassembly")
+        BuildingService:UnlockBuilding("buildings/main/mass_disassembly_by_rarity")
+        BuildingService:UnlockBuilding("buildings/main/mass_disassembly_equal_and_lower")
+        
+        BuildingService:UnlockBuilding("buildings/tools/mass_disassembly_by_rarity_tool")
+    end
+
+    local playerId = evt:GetPlayerId()
+
+    local player = PlayerService:GetGlobalPlayerEntity( playerId )
+    if ( player == nil or player == INVALID_ID ) then
         return
     end
 
-    BuildingService:UnlockBuilding("buildings/main/mass_disassembly")
-    BuildingService:UnlockBuilding("buildings/main/mass_disassembly_by_rarity")
-    BuildingService:UnlockBuilding("buildings/main/mass_disassembly_equal_and_lower")
-        
-    BuildingService:UnlockBuilding("buildings/tools/mass_disassembly_by_rarity_tool")
+
+    local itemList = {
+        "items/mass_disassembly_rarity/standard",
+        "items/mass_disassembly_rarity/advanced",
+        "items/mass_disassembly_rarity/superior",
+        "items/mass_disassembly_rarity/extreme"  
+    }
+
+    local hashItemInInventory = {}
+
+    for itemName in Iter( itemList ) do
+
+        hashItemInInventory[itemName] = false
+    end
+
+    local inventoryComponent = EntityService:GetComponent(player, "InventoryComponent")
+    if ( inventoryComponent ~= nil ) then
+
+        local inventoryComponentRef = reflection_helper( inventoryComponent )
+
+        --LogService:Log(eventName .. " inventoryComponentRef " .. tostring(inventoryComponentRef))
+
+        if ( inventoryComponentRef.inventory ~= nil and inventoryComponentRef.inventory.items ~= nil and inventoryComponentRef.inventory.items.count > 0 ) then
+
+            local items = inventoryComponentRef.inventory.items
+
+            for i=1,items.count do
+
+                local item = items[i]
+
+                if ( item and item.id ~= nil ) then
+
+                    local blueprintName = EntityService:GetBlueprintName(item.id)
+
+                    if ( hashItemInInventory[blueprintName] ~= nil ) then
+
+                        --LogService:Log(eventName .. " blueprintName " .. tostring(blueprintName) .. " EXIST " .. tostring(item.id))
+
+                        hashItemInInventory[blueprintName] = true
+                    end
+                end
+            end
+        end
+
+        if ( inventoryComponentRef.inventory ~= nil and inventoryComponentRef.inventory.items_by_blueprint ~= nil and inventoryComponentRef.inventory.items_by_blueprint.count > 0 ) then
+
+            local items_by_blueprint = inventoryComponentRef.inventory.items_by_blueprint
+
+            for i=1,items_by_blueprint.count do
+
+                local keyCollection = items_by_blueprint[i]
+
+                if ( keyCollection and keyCollection.key ~= nil and keyCollection.value and keyCollection.value.count > 0 ) then
+
+                    local blueprintName = keyCollection.key
+
+                    if ( hashItemInInventory[blueprintName] ~= nil ) then
+
+                        --LogService:Log(eventName .. " items_by_blueprint.key " .. tostring(blueprintName) .. " EXIST ")
+
+                        hashItemInInventory[blueprintName] = true
+                    end
+                end
+            end
+        end
+
+        for itemName in Iter( itemList ) do
+
+            if (hashItemInInventory[itemName] == false) then
+
+                --LogService:Log(eventName .. " itemName " .. tostring(itemName) .. " CREATING.")
+    
+                PlayerService:AddItemToInventory( playerId, itemName )
+            end
+        end
+    end
 end
 
 RegisterGlobalEventHandler("PlayerCreatedEvent", function(evt)
